dcac52c0093c9645adde75dc9756347dce1698c5
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-08-03 12:17:02 +0530

    english added

---
+++ English/script.js
@@ -0,0 +1,107 @@
+const audio = document.getElementById("audio");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const progress = document.getElementById("progress");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const coverImg = document.getElementById("cover");
+const loopBtn = document.getElementById("loop");
+
+const songs = [
+  {
+    name: "songs/beggin.mp3",
+    title: "Beggin",
+    artist: "Artist: Måneskin",
+    cover: "covers/beggin.jpg"
+  },
+  {
+    name: "songs/feeling_good.mp3",
+    title: "Michael Bublé - Feeling Good",
+    artist: "Artist: Michael Bublé",
+    cover: "covers/feeling_good.jpg"
+  },
+  {
+    name: "rahul/songs/Perfect (Acoustic).mp3",
+    title: "Perfect (Acoustic)",
+    artist: "",
+    cover: "rahul/covers/perf.jpg"
+  },
+  {
+    name: "rahul/songs/Stephen Sanchez - Until I Found You (Official Video).mp3",
+    title: "Stephen Sanchez - Until I Found You (Official Video)",
+    artist: "",
+    cover: "rahul/covers/step.jpg"
+  }
+];
+
+let songIndex = 0;
+let isLooping = false;
+
+function loadSong(song) {
+  title.textContent = song.title;
+  artist.textContent = song.artist;
+  audio.src = "../" + song.name;
+  coverImg.src = "../" + song.cover;
+  resetProgress();
+}
+
+function resetProgress() {
+  progress.value = 0;
+}
+
+loadSong(songs[songIndex]);
+
+playBtn.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    playBtn.textContent = "⏸️";
+  } else {
+    audio.pause();
+    playBtn.textContent = "▶️";
+  }
+});
+
+nextBtn.addEventListener("click", () => {
+  songIndex = (songIndex + 1) % songs.length;
+  loadSong(songs[songIndex]);
+  audio.play();
+  playBtn.textContent = "⏸️";
+});
+
+prevBtn.addEventListener("click", () => {
+  songIndex = (songIndex - 1 + songs.length) % songs.length;
+  loadSong(songs[songIndex]);
+  audio.play();
+  playBtn.textContent = "⏸️";
+});
+
+//progress bar update while playing song
+audio.addEventListener("timeupdate", () => {
+  if (audio.duration) {
+    progress.value = (audio.currentTime / audio.duration) * 100;
+  }
+});
+
+//functionality
+progress.addEventListener("input", () => {
+  if (audio.duration) {
+    audio.currentTime = (progress.value * audio.duration) / 100;
+  }
+});
+
+audio.addEventListener("ended", () => {
+  if (!isLooping) {
+    songIndex = (songIndex + 1) % songs.length;
+    loadSong(songs[songIndex]);
+    audio.play();
+  }
+});
+
+loopBtn.addEventListener("click", () => {
+  isLooping = !isLooping;
+  audio.loop = isLooping;
+
+  // Update button text
+  loopBtn.textContent = isLooping ? "🔁 Loop On" : "🔁 Loop Off";
+});
