f76c82872db2f1014f30f5851f6bf05b2030d740
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-08-07 03:13:27 +0530

    songs added

--- Indrajit/js/songs.js
+++ Indrajit/js/songs.js
@@ -1,44 +1,86 @@
 const songs = [
 {
-  name: "Imagine_Dragons_-_Bones.mp3",
-  title: "Imagine Dragons - Bones",
+  name: "Ed_Sheeran_-_Perfect.mp3",
+  title: "Ed Sheeran - Perfect",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Bones.jpg"
+  cover: "covers/Ed_Sheeran_-_Perfect.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Bad_Liar.mp3",
-  title: "Imagine Dragons - Bad Liar",
+  name: "Ed_Sheeran_-_Photograph.mp3",
+  title: "Ed Sheeran - Photograph",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Bad_Liar.jpg"
+  cover: "covers/Ed_Sheeran_-_Photograph.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Demons.mp3",
-  title: "Imagine Dragons - Demons",
+  name: "Ed_Sheeran_-_Shape_of_You.mp3",
+  title: "Ed Sheeran - Shape of You",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Demons.jpg"
+  cover: "covers/Ed_Sheeran_-_Shape_of_You.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Believer.mp3",
-  title: "Imagine Dragons - Believer",
+  name: "Ed_Sheeran_-_I_Don't_Care_Ed_Sheeran_-_Beautiful_People.mp3",
+  title: "Ed Sheeran - I Don't Care Ed Sheeran - Beautiful People",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Believer.jpg"
+  cover: "covers/Ed_Sheeran_-_I_Don't_Care_Ed_Sheeran_-_Beautiful_People.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Take_me_to_the_beach.mp3",
-  title: "Imagine Dragons - Take me to the beach",
+  name: "Ed_Sheeran_-_Thinking_Out_Loud.mp3",
+  title: "Ed Sheeran - Thinking Out Loud",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Take_me_to_the_beach.jpg"
+  cover: "covers/Ed_Sheeran_-_Thinking_Out_Loud.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Enemy.mp3",
-  title: "Imagine Dragons - Enemy",
+  name: "Passenger_-_Let_Her_Go.mp3",
+  title: "Passenger - Let Her Go",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Enemy.jpg"
+  cover: "covers/Passenger_-_Let_Her_Go.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Thunder.mp3",
-  title: "Imagine Dragons - Thunder",
+  name: "Bruno_Mars_-_Talking_To_The_Moon.mp3",
+  title: "Bruno Mars - Talking To The Moon",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Thunder.jpg"
+  cover: "covers/Bruno_Mars_-_Talking_To_The_Moon.jpg"
+},
+{
+  name: "Marron_5_-_Girls_Like_You.mp3",
+  title: "Marron 5 - Girls Like You",
+  artist: "Artist: Unknown",
+  cover: "covers/Marron_5_-_Girls_Like_You.jpg"
+},
+{
+  name: "Post_Malone_-_Sunflower.mp3",
+  title: "Post Malone - Sunflower",
+  artist: "Artist: Unknown",
+  cover: "covers/Post_Malone_-_Sunflower.jpg"
+},
+{
+  name: "Avicii_-_The_Nights.mp3",
+  title: "Avicii - The Nights",
+  artist: "Artist: Unknown",
+  cover: "covers/Avicii_-_The_Nights.jpg"
+},
+{
+  name: "Avicii_-_Waiting_For_Love.mp3",
+  title: "Avicii - Waiting For Love",
+  artist: "Artist: Unknown",
+  cover: "covers/Avicii_-_Waiting_For_Love.jpg"
+},
+{
+  name: "The_Chainsmokers_-_Closer.mp3",
+  title: "The Chainsmokers - Closer",
+  artist: "Artist: Unknown",
+  cover: "covers/The_Chainsmokers_-_Closer.jpg"
+},
+{
+  name: "Charlie_Puth_-_One_Call_Away.mp3",
+  title: "Charlie Puth - One Call Away",
+  artist: "Artist: Unknown",
+  cover: "covers/Charlie_Puth_-_One_Call_Away.jpg"
+},
+{
+  name: "Charlie_Puth_-_We_Don't_Talk_Anymore.mp3",
+  title: "Charlie Puth - We Don't Talk Anymore",
+  artist: "Artist: Unknown",
+  cover: "covers/Charlie_Puth_-_We_Don't_Talk_Anymore.jpg"
 }
 ];
