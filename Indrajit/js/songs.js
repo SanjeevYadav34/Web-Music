a8c04084bc96e8c2a375f63a22d6cc598214fb5e
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-08-07 00:24:12 +0530

    added

---
+++ Indrajit/js/songs.js
@@ -0,0 +1,44 @@
+const songs = [
+{
+  name: "Imagine_Dragons_-_Bones.mp3",
+  title: "Imagine Dragons - Bones",
+  artist: "Artist: Unknown",
+  cover: "covers/Imagine_Dragons_-_Bones.jpg"
+},
+{
+  name: "Imagine_Dragons_-_Bad_Liar.mp3",
+  title: "Imagine Dragons - Bad Liar",
+  artist: "Artist: Unknown",
+  cover: "covers/Imagine_Dragons_-_Bad_Liar.jpg"
+},
+{
+  name: "Imagine_Dragons_-_Demons.mp3",
+  title: "Imagine Dragons - Demons",
+  artist: "Artist: Unknown",
+  cover: "covers/Imagine_Dragons_-_Demons.jpg"
+},
+{
+  name: "Imagine_Dragons_-_Believer.mp3",
+  title: "Imagine Dragons - Believer",
+  artist: "Artist: Unknown",
+  cover: "covers/Imagine_Dragons_-_Believer.jpg"
+},
+{
+  name: "Imagine_Dragons_-_Take_me_to_the_beach.mp3",
+  title: "Imagine Dragons - Take me to the beach",
+  artist: "Artist: Unknown",
+  cover: "covers/Imagine_Dragons_-_Take_me_to_the_beach.jpg"
+},
+{
+  name: "Imagine_Dragons_-_Enemy.mp3",
+  title: "Imagine Dragons - Enemy",
+  artist: "Artist: Unknown",
+  cover: "covers/Imagine_Dragons_-_Enemy.jpg"
+},
+{
+  name: "Imagine_Dragons_-_Thunder.mp3",
+  title: "Imagine Dragons - Thunder",
+  artist: "Artist: Unknown",
+  cover: "covers/Imagine_Dragons_-_Thunder.jpg"
+}
+];
