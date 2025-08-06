34e6c7c398426b7d79d10c7cf8551b140c730d3d
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-06 07:35:27 +0530

    Update script.js

--- Vanita/script.js
+++ Vanita/script.js
@@ -68,6 +68,66 @@ const songs = [
   title: "In Dino Pritam Soham",
   artist: "Artist: Unknown",
   cover: "covers/In_Dino_Pritam_Soham.jpg"
+},
+{
+  name: "Bade_Achhe_Lagte_Hain_Amit_Kumar_R._D._Burman.mp3",
+  title: "Bade Achhe Lagte Hain Amit Kumar R. D. Burman",
+  artist: "Artist: Unknown",
+  cover: "covers/Bade_Achhe_Lagte_Hain_Amit_Kumar_R._D._Burman.jpg"
+},
+{
+  name: "Sanson_Ki_Mala_Peh_Simroon_Nusrat_Fateh_Ali_Khan.mp3",
+  title: "Sanson Ki Mala Peh Simroon Nusrat Fateh Ali Khan",
+  artist: "Artist: Unknown",
+  cover: "covers/Sanson_Ki_Mala_Peh_Simroon_Nusrat_Fateh_Ali_Khan.jpg"
+},
+{
+  name: "Suno_Na_Suno_Na_Abhijeet.mp3",
+  title: "Suno Na Suno Na Abhijeet",
+  artist: "Artist: Unknown",
+  cover: "covers/Suno_Na_Suno_Na_Abhijeet.jpg"
+},
+{
+  name: "Main_Koi_Aisa_Geet_Gaoon_Abhijeet_Alka_Yagnik.mp3",
+  title: "Main Koi Aisa Geet Gaoon Abhijeet Alka Yagnik",
+  artist: "Artist: Unknown",
+  cover: "covers/Main_Koi_Aisa_Geet_Gaoon_Abhijeet_Alka_Yagnik.jpg"
+},
+{
+  name: "Soniyo_Raju_Singh_Sonu_Nigam.mp3",
+  title: "Soniyo Raju Singh Sonu Nigam",
+  artist: "Artist: Unknown",
+  cover: "covers/Soniyo_Raju_Singh_Sonu_Nigam.jpg"
+},
+{
+  name: "Jadu_Hai_Nasha_Hai_2002_Shreya_Ghoshal_Neelesh_Misra.mp3",
+  title: "Jadu Hai Nasha Hai 2002 Shreya Ghoshal Neelesh Misra",
+  artist: "Artist: Unknown",
+  cover: "covers/Jadu_Hai_Nasha_Hai_2002_Shreya_Ghoshal_Neelesh_Misra.jpg"
+},
+{
+  name: "Jiyein_Kyun_Pritam_Papon.mp3",
+  title: "Jiyein Kyun Pritam Papon",
+  artist: "Artist: Unknown",
+  cover: "covers/Jiyein_Kyun_Pritam_Papon.jpg"
+},
+{
+  name: "Bin_Tere_Vishal-Shekhar_Shafqat_Amanat_Ali.mp3",
+  title: "Bin Tere Vishal-Shekhar Shafqat Amanat Ali",
+  artist: "Artist: Unknown",
+  cover: "covers/Bin_Tere_Vishal-Shekhar_Shafqat_Amanat_Ali.jpg"
+},
+{
+  name: "Saware_Pritam_Arijit_Singh.mp3",
+  title: "Saware Pritam Arijit Singh",
+  artist: "Artist: Unknown",
+  cover: "covers/Saware_Pritam_Arijit_Singh.jpg"
+},
+{
+  name: "Tu_Hi_Re_From_Bombay_Hariharan_Kavita_Krishnamurthy.mp3",
+  title: "Tu Hi Re From Bombay Hariharan Kavita Krishnamurthy",
+  artist: "Artist: Unknown",
+  cover: "covers/Tu_Hi_Re_From_Bombay_Hariharan_Kavita_Krishnamurthy.jpg"
 }
 ];
 
