1d6ea22d5f2ee19c0a80300905a871f517493717
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-08-07 04:52:31 +0530

    song

--- Indrajit/js/songs.js
+++ Indrajit/js/songs.js
@@ -1,86 +1,8 @@
 const songs = [
 {
-  name: "Ed_Sheeran_-_Perfect.mp3",
-  title: "Ed Sheeran - Perfect",
+  name: "Ed_-_Sheeran_-_I_Don't_Care.mp3",
+  title: "Ed - Sheeran - I Don't Care",
   artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Perfect.jpg"
-},
-{
-  name: "Ed_Sheeran_-_Photograph.mp3",
-  title: "Ed Sheeran - Photograph",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Photograph.jpg"
-},
-{
-  name: "Ed_Sheeran_-_Shape_of_You.mp3",
-  title: "Ed Sheeran - Shape of You",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Shape_of_You.jpg"
-},
-{
-  name: "Ed_Sheeran_-_I_Don't_Care_Ed_Sheeran_-_Beautiful_People.mp3",
-  title: "Ed Sheeran - I Don't Care Ed Sheeran - Beautiful People",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_I_Don't_Care_Ed_Sheeran_-_Beautiful_People.jpg"
-},
-{
-  name: "Ed_Sheeran_-_Thinking_Out_Loud.mp3",
-  title: "Ed Sheeran - Thinking Out Loud",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Thinking_Out_Loud.jpg"
-},
-{
-  name: "Passenger_-_Let_Her_Go.mp3",
-  title: "Passenger - Let Her Go",
-  artist: "Artist: Unknown",
-  cover: "covers/Passenger_-_Let_Her_Go.jpg"
-},
-{
-  name: "Bruno_Mars_-_Talking_To_The_Moon.mp3",
-  title: "Bruno Mars - Talking To The Moon",
-  artist: "Artist: Unknown",
-  cover: "covers/Bruno_Mars_-_Talking_To_The_Moon.jpg"
-},
-{
-  name: "Marron_5_-_Girls_Like_You.mp3",
-  title: "Marron 5 - Girls Like You",
-  artist: "Artist: Unknown",
-  cover: "covers/Marron_5_-_Girls_Like_You.jpg"
-},
-{
-  name: "Post_Malone_-_Sunflower.mp3",
-  title: "Post Malone - Sunflower",
-  artist: "Artist: Unknown",
-  cover: "covers/Post_Malone_-_Sunflower.jpg"
-},
-{
-  name: "Avicii_-_The_Nights.mp3",
-  title: "Avicii - The Nights",
-  artist: "Artist: Unknown",
-  cover: "covers/Avicii_-_The_Nights.jpg"
-},
-{
-  name: "Avicii_-_Waiting_For_Love.mp3",
-  title: "Avicii - Waiting For Love",
-  artist: "Artist: Unknown",
-  cover: "covers/Avicii_-_Waiting_For_Love.jpg"
-},
-{
-  name: "The_Chainsmokers_-_Closer.mp3",
-  title: "The Chainsmokers - Closer",
-  artist: "Artist: Unknown",
-  cover: "covers/The_Chainsmokers_-_Closer.jpg"
-},
-{
-  name: "Charlie_Puth_-_One_Call_Away.mp3",
-  title: "Charlie Puth - One Call Away",
-  artist: "Artist: Unknown",
-  cover: "covers/Charlie_Puth_-_One_Call_Away.jpg"
-},
-{
-  name: "Charlie_Puth_-_We_Don't_Talk_Anymore.mp3",
-  title: "Charlie Puth - We Don't Talk Anymore",
-  artist: "Artist: Unknown",
-  cover: "covers/Charlie_Puth_-_We_Don't_Talk_Anymore.jpg"
+  cover: "covers/Ed_-_Sheeran_-_I_Don't_Care.jpg"
 }
 ];
