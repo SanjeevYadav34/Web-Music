d8bbcc5437e82afe80c39fd020908927a23c4e35
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-08 04:58:33 +0530

    Update script.js

--- Sagar/script.js
+++ Sagar/script.js
@@ -10,130 +10,34 @@ const loopBtn = document.getElementById("loop");
 
 const songs = [
 {
-  name: "Imagine_Dragons_-_Bones.mp3",
-  title: "Imagine Dragons - Bones",
+  name: "namo_namo_song.mp3",
+  title: "namo namo song",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Bones.jpg"
+  cover: "covers/namo_namo_song.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Bad_Liar.mp3",
-  title: "Imagine Dragons - Bad Liar",
+  name: "shiv_tandav_song.mp3",
+  title: "shiv tandav song",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Bad_Liar.jpg"
+  cover: "covers/shiv_tandav_song.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Demons.mp3",
-  title: "Imagine Dragons - Demons",
+  name: "hanuman_Chalisa_song.mp3",
+  title: "hanuman Chalisa song",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Demons.jpg"
+  cover: "covers/hanuman_Chalisa_song.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Believer.mp3",
-  title: "Imagine Dragons - Believer",
+  name: "Ganpati_aarti_song.mp3",
+  title: "Ganpati aarti song",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Believer.jpg"
+  cover: "covers/Ganpati_aarti_song.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Enemy.mp3",
-  title: "Imagine Dragons - Enemy",
+  name: "aai_dev_bappa_aale_song.mp3",
+  title: "aai dev bappa aale song",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Enemy.jpg"
-},
-{
-  name: "Imagine_Dragons_-_Thunder.mp3",
-  title: "Imagine Dragons - Thunder",
-  artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Thunder.jpg"
-},
-{
-  name: "Ed_Sheeran_-_Perfect.mp3",
-  title: "Ed Sheeran - Perfect",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Perfect.jpg"
-},
-{
-  name: "Ed_Sheeran_-_Photograph.mp3",
-  title: "Ed Sheeran - Photograph",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Photograph.jpg"
-},
-{
-  name: "Ed_Sheeran_-_Shape_of_You.mp3",
-  title: "Ed Sheeran - Shape of You",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Shape_of_You.jpg"
-},
-{
-  name: "Ed_-_Sheeran_-_I_Don't_Care.mp3",
-  title: "Ed - Sheeran - I Don't Care",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_-_Sheeran_-_I_Don't_Care.jpg"
-},
-{
-  name: "Ed_Sheeran_-_I_Don't_Care_Ed_Sheeran_-_Beautiful_People.mp3",
-  title: "Ed Sheeran - Beautiful People",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_I_Don't_Care_Ed_Sheeran_-_Beautiful_People.jpg"
-},
-{
-  name: "Ed_Sheeran_-_Thinking_Out_Loud.mp3",
-  title: "Ed Sheeran - Thinking Out Loud",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Thinking_Out_Loud.jpg"
-},
-{
-  name: "Passenger_-_Let_Her_Go.mp3",
-  title: "Passenger - Let Her Go",
-  artist: "Artist: Unknown",
-  cover: "covers/Passenger_-_Let_Her_Go.jpg"
-},
-{
-  name: "Bruno_Mars_-_Talking_To_The_Moon.mp3",
-  title: "Bruno Mars - Talking To The Moon",
-  artist: "Artist: Unknown",
-  cover: "covers/Bruno_Mars_-_Talking_To_The_Moon.jpg"
-},
-{
-  name: "Marron_5_-_Girls_Like_You.mp3",
-  title: "Marron 5 - Girls Like You",
-  artist: "Artist: Unknown",
-  cover: "covers/Marron_5_-_Girls_Like_You.jpg"
-},
-{
-  name: "Post_Malone_-_Sunflower.mp3",
-  title: "Post Malone - Sunflower",
-  artist: "Artist: Unknown",
-  cover: "covers/Post_Malone_-_Sunflower.jpg"
-},
-{
-  name: "Avicii_-_The_Nights.mp3",
-  title: "Avicii - The Nights",
-  artist: "Artist: Unknown",
-  cover: "covers/Avicii_-_The_Nights.jpg"
-},
-{
-  name: "Avicii_-_Waiting_For_Love.mp3",
-  title: "Avicii - Waiting For Love",
-  artist: "Artist: Unknown",
-  cover: "covers/Avicii_-_Waiting_For_Love.jpg"
-},
-{
-  name: "The_Chainsmokers_-_Closer.mp3",
-  title: "The Chainsmokers - Closer",
-  artist: "Artist: Unknown",
-  cover: "covers/The_Chainsmokers_-_Closer.jpg"
-},
-{
-  name: "Charlie_Puth_-_One_Call_Away.mp3",
-  title: "Charlie Puth - One Call Away",
-  artist: "Artist: Unknown",
-  cover: "covers/Charlie_Puth_-_One_Call_Away.jpg"
-},
-{
-  name: "Charlie_Puth_-_We_Don't_Talk_Anymore.mp3",
-  title: "Charlie Puth - We Don't Talk Anymore",
-  artist: "Artist: Unknown",
-  cover: "covers/Charlie_Puth_-_We_Don't_Talk_Anymore.jpg"
+  cover: "covers/aai_dev_bappa_aale_song.jpg"
 }
 ];
 
