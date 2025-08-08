91ed9e1171e6519eda67fdddb63d6f4fd719e3bc
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-08 07:18:09 +0530

    Update script.js

--- English/script.js
+++ English/script.js
@@ -51,12 +51,132 @@ const songs = [
     artist: "Artist: Michael Bublé",
     cover: "covers/feeling_good.jpg"
   },
-  {
-    name: "rahul/songs/Perfect (Acoustic).mp3",
-    title: "Perfect (Acoustic)",
-    artist: "",
-    cover: "rahul/covers/perf.jpg"
-  },
+{
+  name: "Indrajit/songs/Imagine_Dragons_-_Bones.mp3",
+  title: "Imagine Dragons - Bones",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Imagine_Dragons_-_Bones.jpg"
+},
+{
+  name: "Indrajit/songs/Imagine_Dragons_-_Bad_Liar.mp3",
+  title: "Imagine Dragons - Bad Liar",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Imagine_Dragons_-_Bad_Liar.jpg"
+},
+{
+  name: "Indrajit/songs/Imagine_Dragons_-_Demons.mp3",
+  title: "Imagine Dragons - Demons",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Imagine_Dragons_-_Demons.jpg"
+},
+{
+  name: "Indrajit/songs/Imagine_Dragons_-_Believer.mp3",
+  title: "Imagine Dragons - Believer",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Imagine_Dragons_-_Believer.jpg"
+},
+{
+  name: "Indrajit/songs/Imagine_Dragons_-_Enemy.mp3",
+  title: "Imagine Dragons - Enemy",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Imagine_Dragons_-_Enemy.jpg"
+},
+{
+  name: "Indrajit/songs/Imagine_Dragons_-_Thunder.mp3",
+  title: "Imagine Dragons - Thunder",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Imagine_Dragons_-_Thunder.jpg"
+},
+{
+  name: "Indrajit/songs/Ed_Sheeran_-_Perfect.mp3",
+  title: "Ed Sheeran - Perfect",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Ed_Sheeran_-_Perfect.jpg"
+},
+{
+  name: "Indrajit/songs/Ed_Sheeran_-_Photograph.mp3",
+  title: "Ed Sheeran - Photograph",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Ed_Sheeran_-_Photograph.jpg"
+},
+{
+  name: "Indrajit/songs/Ed_Sheeran_-_Shape_of_You.mp3",
+  title: "Ed Sheeran - Shape of You",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Ed_Sheeran_-_Shape_of_You.jpg"
+},
+{
+  name: "Indrajit/songs/Ed_-_Sheeran_-_I_Don't_Care.mp3",
+  title: "Ed - Sheeran - I Don't Care",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Ed_-_Sheeran_-_I_Don't_Care.jpg"
+},
+{
+  name: "Indrajit/songs/Ed_Sheeran_-_I_Don't_Care_Ed_Sheeran_-_Beautiful_People.mp3",
+  title: "Ed Sheeran - Beautiful People",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Ed_Sheeran_-_I_Don't_Care_Ed_Sheeran_-_Beautiful_People.jpg"
+},
+{
+  name: "Indrajit/songs/Ed_Sheeran_-_Thinking_Out_Loud.mp3",
+  title: "Ed Sheeran - Thinking Out Loud",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Ed_Sheeran_-_Thinking_Out_Loud.jpg"
+},
+{
+  name: "Indrajit/songs/Passenger_-_Let_Her_Go.mp3",
+  title: "Passenger - Let Her Go",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Passenger_-_Let_Her_Go.jpg"
+},
+{
+  name: "Indrajit/songs/Bruno_Mars_-_Talking_To_The_Moon.mp3",
+  title: "Bruno Mars - Talking To The Moon",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Bruno_Mars_-_Talking_To_The_Moon.jpg"
+},
+{
+  name: "Indrajit/songs/Marron_5_-_Girls_Like_You.mp3",
+  title: "Marron 5 - Girls Like You",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Marron_5_-_Girls_Like_You.jpg"
+},
+{
+  name: "Indrajit/songs/Post_Malone_-_Sunflower.mp3",
+  title: "Post Malone - Sunflower",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Post_Malone_-_Sunflower.jpg"
+},
+{
+  name: "Indrajit/songs/Avicii_-_The_Nights.mp3",
+  title: "Avicii - The Nights",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Avicii_-_The_Nights.jpg"
+},
+{
+  name: "Indrajit/songs/Avicii_-_Waiting_For_Love.mp3",
+  title: "Avicii - Waiting For Love",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Avicii_-_Waiting_For_Love.jpg"
+},
+{
+  name: "Indrajit/songs/The_Chainsmokers_-_Closer.mp3",
+  title: "The Chainsmokers - Closer",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/The_Chainsmokers_-_Closer.jpg"
+},
+{
+  name: "Indrajit/songs/Charlie_Puth_-_One_Call_Away.mp3",
+  title: "Charlie Puth - One Call Away",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Charlie_Puth_-_One_Call_Away.jpg"
+},
+{
+  name: "Indrajit/songs/Charlie_Puth_-_We_Don't_Talk_Anymore.mp3",
+  title: "Charlie Puth - We Don't Talk Anymore",
+  artist: "Artist: Unknown",
+  cover: "Indrajit/covers/Charlie_Puth_-_We_Don't_Talk_Anymore.jpg"
+},
   {
     name: "rahul/songs/Stephen Sanchez - Until I Found You (Official Video).mp3",
     title: "Stephen Sanchez - Until I Found You (Official Video)",
