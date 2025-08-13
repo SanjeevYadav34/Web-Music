76d492ade2a8b86e1a49d681ac4ba50b0cdef914
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-13 13:51:22 +0530

    Update script.js

--- Indrajit/Hindi/script.js
+++ Indrajit/Hindi/script.js
@@ -92,6 +92,108 @@ const songs = [
   title: "Wake Up Sid - Aaj Kal Zindagi",
   artist: "Artist: Unknown",
   cover: "covers/Wake_Up_Sid_-_Aaj_Kal_Zindagi.jpg"
+},
+{
+  name: "Yeh_Jawani_Hai_Deewani_-_Badtameez_Dil.mp3",
+  title: "Yeh Jawani Hai Deewani - Badtameez Dil",
+  artist: "Artist: Unknown",
+  cover: "covers/Yeh_Jawani_Hai_Deewani_-_Badtameez_Dil.jpg"
+},
+{
+  name: "Yeh_Jawani_Hai_Deewani_-_Ilahi_(Arijit_Singh).mp3",
+  title: "Yeh Jawani Hai Deewani - Ilahi (Arijit Singh)",
+  artist: "Artist: Unknown",
+  cover: "covers/Yeh_Jawani_Hai_Deewani_-_Ilahi_(Arijit_Singh).jpg"
+},
+{
+  name: "Yeh_Jawani_Hai_Deewani_-_Ilahi_(Reprise_Mohit_Chauhan).mp3",
+  title: "Yeh Jawani Hai Deewani - Ilahi (Reprise Mohit Chauhan)",
+  artist: "Artist: Unknown",
+  cover: "covers/Yeh_Jawani_Hai_Deewani_-_Ilahi_(Reprise_Mohit_Chauhan).jpg"
+},
+{
+  name: "Yeh_Jawani_Hai_Deewani_-_Kabira.mp3",
+  title: "Yeh Jawani Hai Deewani - Kabira",
+  artist: "Artist: Unknown",
+  cover: "covers/Yeh_Jawani_Hai_Deewani_-_Kabira.jpg"
+},
+{
+  name: "Yeh_Jawani_Hai_Deewani_-_Kabira_(Encore).mp3",
+  title: "Yeh Jawani Hai Deewani - Kabira (Encore)",
+  artist: "Artist: Unknown",
+  cover: "covers/Yeh_Jawani_Hai_Deewani_-_Kabira_(Encore).jpg"
+},
+{
+  name: "Yeh_Jawani_Hai_Deewani_-_Subhanallah.mp3",
+  title: "Yeh Jawani Hai Deewani - Subhanallah",
+  artist: "Artist: Unknown",
+  cover: "covers/Yeh_Jawani_Hai_Deewani_-_Subhanallah.jpg"
+},
+{
+  name: "Ae_Dil_Hai_Mushkil_Song.mp3",
+  title: "Ae Dil Hai Mushkil Song",
+  artist: "Artist: Unknown",
+  cover: "covers/Ae_Dil_Hai_Mushkil_Song.jpg"
+},
+{
+  name: "Ae_Dil_Hai_Mushkil_-_Bulleya.mp3",
+  title: "Ae Dil Hai Mushkil - Bulleya",
+  artist: "Artist: Unknown",
+  cover: "covers/Ae_Dil_Hai_Mushkil_-_Bulleya.jpg"
+},
+{
+  name: "Ae_Dil_Hai_Mushkil_-_Channa_Mereya.mp3",
+  title: "Ae Dil Hai Mushkil - Channa Mereya",
+  artist: "Artist: Unknown",
+  cover: "covers/Ae_Dil_Hai_Mushkil_-_Channa_Mereya.jpg"
+},
+{
+  name: "Ae_Dil_Hai_Mushkil_-_Aaj_Jane_Ki_Zid_Na_Karo.mp3",
+  title: "Ae Dil Hai Mushkil - Aaj Jane Ki Zid Na Karo",
+  artist: "Artist: Unknown",
+  cover: "covers/Ae_Dil_Hai_Mushkil_-_Aaj_Jane_Ki_Zid_Na_Karo.jpg"
+},
+{
+  name: "Dil_Chahta_Hai_Song.mp3",
+  title: "Dil Chahta Hai Song",
+  artist: "Artist: Unknown",
+  cover: "covers/Dil_Chahta_Hai_Song.jpg"
+},
+{
+  name: "Dil_Chahta_Hai_(Reprise).mp3",
+  title: "Dil Chahta Hai (Reprise)",
+  artist: "Artist: Unknown",
+  cover: "covers/Dil_Chahta_Hai_(Reprise).jpg"
+},
+{
+  name: "Dil_Chahta_Hai_-_Tanhayee.mp3",
+  title: "Dil Chahta Hai - Tanhayee",
+  artist: "Artist: Unknown",
+  cover: "covers/Dil_Chahta_Hai_-_Tanhayee.jpg"
+},
+{
+  name: "Dil_Chahta_Hai_-_Jaane_Kyon_Log_Pyar.mp3",
+  title: "Dil Chahta Hai - Jaane Kyon Log Pyar",
+  artist: "Artist: Unknown",
+  cover: "covers/Dil_Chahta_Hai_-_Jaane_Kyon_Log_Pyar.jpg"
+},
+{
+  name: "Dil_Chahta_Hai_-_Kaisi_Hai_Ye_Rut.mp3",
+  title: "Dil Chahta Hai - Kaisi Hai Ye Rut",
+  artist: "Artist: Unknown",
+  cover: "covers/Dil_Chahta_Hai_-_Kaisi_Hai_Ye_Rut.jpg"
+},
+{
+  name: "Dil_Chahta_Hai_-_Koi_Kahe_Kehta_Rahe.mp3",
+  title: "Dil Chahta Hai - Koi Kahe Kehta Rahe",
+  artist: "Artist: Unknown",
+  cover: "covers/Dil_Chahta_Hai_-_Koi_Kahe_Kehta_Rahe.jpg"
+},
+{
+  name: "Dil_Chahta_Hai_-_Woh_Ladki_Hai_Kahan.mp3",
+  title: "Dil Chahta Hai - Woh Ladki Hai Kahan",
+  artist: "Artist: Unknown",
+  cover: "covers/Dil_Chahta_Hai_-_Woh_Ladki_Hai_Kahan.jpg"
 }
 ];
 
