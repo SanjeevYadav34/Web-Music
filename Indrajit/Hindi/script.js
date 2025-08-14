dbd653abaadb4af99016d5c8cb8a3fe7bfd65d77
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-15 05:03:57 +0530

    Update script.js

--- Indrajit/Hindi/script.js
+++ Indrajit/Hindi/script.js
@@ -284,6 +284,12 @@ const songs = [
   title: "Kedarnath - Jaan Nisaar (Asees)",
   artist: "Artist: Unknown",
   cover: "covers/Kedarnath_-_Jaan_Nisaar_(Asees).jpg"
+},
+{
+  name: "Amit_Trivedi_-_Naina_Da_Kya_Kasoor.mp3",
+  title: "Amit Trivedi - Naina Da Kya Kasoor",
+  artist: "Artist: Unknown",
+  cover: "covers/Amit_Trivedi_-_Naina_Da_Kya_Kasoor.jpg"
 }
 ];
 
