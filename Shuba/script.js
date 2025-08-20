4143b9f87ddd89234cb260ef36e94eb8ac312f57
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-21 05:16:18 +0530

    Update script.js

--- Shuba/script.js
+++ Shuba/script.js
@@ -494,6 +494,126 @@ const songs = [
   title: "Anirudh Ravichander - Takkunu Takkunu",
   artist: "Artist: Unknown",
   cover: "covers/Anirudh_Ravichander_-_Takkunu_Takkunu.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Pudhusaattam.mp3",
+  title: "Anirudh Ravichander - Pudhusaattam",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Pudhusaattam.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Kanne_Kanne.mp3",
+  title: "Anirudh Ravichander - Kanne Kanne",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Kanne_Kanne.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Kadhal_Psycho.mp3",
+  title: "Anirudh Ravichander - Kadhal Psycho",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Kadhal_Psycho.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Sandakari_Neethan.mp3",
+  title: "Anirudh Ravichander - Sandakari Neethan",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Sandakari_Neethan.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Thiruvizha.mp3",
+  title: "Anirudh Ravichander - Thiruvizha",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Thiruvizha.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_I_Want_A_Girl.mp3",
+  title: "Anirudh Ravichander - I Want A Girl",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_I_Want_A_Girl.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Chumma_Kizhi.mp3",
+  title: "Anirudh Ravichander - Chumma Kizhi",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Chumma_Kizhi.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Thani_Vazhi.mp3",
+  title: "Anirudh Ravichander - Thani Vazhi",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Thani_Vazhi.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Tharam_Maara.mp3",
+  title: "Anirudh Ravichander - Tharam Maara",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Tharam_Maara.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Yelo_Pullelo.mp3",
+  title: "Anirudh Ravichander - Yelo Pullelo",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Yelo_Pullelo.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Dharala_Prabhu_(Title_Track).mp3",
+  title: "Anirudh Ravichander - Dharala Prabhu (Title Track)",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Dharala_Prabhu_(Title_Track).jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Vaathi_Raid.mp3",
+  title: "Anirudh Ravichander - Vaathi Raid",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Vaathi_Raid.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Tamizhan_Endru_Sollada.mp3",
+  title: "Anirudh Ravichander - Tamizhan Endru Sollada",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Tamizhan_Endru_Sollada.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Chellamma.mp3",
+  title: "Anirudh Ravichander - Chellamma",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Chellamma.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Nenjame_Nenjame.mp3",
+  title: "Anirudh Ravichander - Nenjame Nenjame",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Nenjame_Nenjame.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_So_Baby.mp3",
+  title: "Anirudh Ravichander - So Baby",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_So_Baby.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Beast_Mode.mp3",
+  title: "Anirudh Ravichander - Beast Mode",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Beast_Mode.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Dippam_Dappam.mp3",
+  title: "Anirudh Ravichander - Dippam Dappam",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Dippam_Dappam.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Kanne_Kanmaniyae.mp3",
+  title: "Anirudh Ravichander - Kanne Kanmaniyae",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Kanne_Kanmaniyae.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Rendu_Kaadhal.mp3",
+  title: "Anirudh Ravichander - Rendu Kaadhal",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Rendu_Kaadhal.jpg"
 }
 ];
 
