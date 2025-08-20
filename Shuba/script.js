5dd39b6fa60e94f758d68be25cc17b5c5c5503c1
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-21 03:07:38 +0530

    Update script.js

--- Shuba/script.js
+++ Shuba/script.js
@@ -194,6 +194,66 @@ const songs = [
   title: "Anirudh Ravichander - Hayyoda",
   artist: "Artist: Unknown",
   cover: "covers/Anirudh_Ravichander_-_Hayyoda.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Come_On_Girls_–_The_Celebration_of_Love.mp3",
+  title: "Anirudh Ravichander - Come On Girls – The Celebration of Love",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Come_On_Girls_–_The_Celebration_of_Love.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Kanave_Kanave.mp3",
+  title: "Anirudh Ravichander - Kanave Kanave",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Kanave_Kanave.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Chennai_City_Gangsta.mp3",
+  title: "Anirudh Ravichander - Chennai City Gangsta",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Chennai_City_Gangsta.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Ailasa_Aile_Ailasa.mp3",
+  title: "Anirudh Ravichander - Ailasa Aile Ailasa",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Ailasa_Aile_Ailasa.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_What_a_Karuvaad.mp3",
+  title: "Anirudh Ravichander - What a Karuvaad",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_What_a_Karuvaad.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Pakkam_Vanthu.mp3",
+  title: "Anirudh Ravichander - Pakkam Vanthu",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Pakkam_Vanthu.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Aathi_Ena_Nee.mp3",
+  title: "Anirudh Ravichander - Aathi Ena Nee",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Aathi_Ena_Nee.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Kadhal_Kan_Kattudhe.mp3",
+  title: "Anirudh Ravichander - Kadhal Kan Kattudhe",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Kadhal_Kan_Kattudhe.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_I’m_So_Cool.mp3",
+  title: "Anirudh Ravichander - I’m So Cool",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_I’m_So_Cool.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Shake_That.mp3",
+  title: "Anirudh Ravichander - Shake That",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Shake_That.jpg"
 }
 ];
 
