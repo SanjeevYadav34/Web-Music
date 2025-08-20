ea08cbcb68b9ce5bc80bcd12109b11a14b3b97c8
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-20 18:01:07 +0530

    Update script.js

--- Shuba/script.js
+++ Shuba/script.js
@@ -128,6 +128,72 @@ const songs = [
   title: "Anirudh Ravichander - Arabic Kuthu (Halamithi Habibo)",
   artist: "Artist: Unknown",
   cover: "covers/Anirudh_Ravichander_-_Arabic_Kuthu_(Halamithi_Habibo).jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Jolly_O_Gymkhana.mp3",
+  title: "Anirudh Ravichander - Jolly O Gymkhana",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Jolly_O_Gymkhana.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Beast_Mode.mp3",
+  title: "Anirudh Ravichander - Beast Mode",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Beast_Mode.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Ranjithame.mp3",
+  title: "Anirudh Ravichander - Ranjithame",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Ranjithame.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Jimikki_Ponnu.mp3",
+  title: "Anirudh Ravichander - Jimikki Ponnu",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Jimikki_Ponnu.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Badass.mp3",
+  title: "Anirudh Ravichander - Badass",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Badass.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Ordinary_Person.mp3",
+  title: "Anirudh Ravichander - Ordinary Person",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Ordinary_Person.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_So_Baby.mp3",
+  title: "Anirudh Ravichander - So Baby",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_So_Baby.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Pathala_Pathala.mp3",
+  title: "Anirudh Ravichander - Pathala Pathala",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Pathala_Pathala.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Vaa_Vaathi.mp3",
+  title: "Anirudh Ravichander - Vaa Vaathi",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Vaa_Vaathi.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Chaleya.mp3",
+  title: "Anirudh Ravichander - Chaleya",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Chaleya.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Hayyoda.mp3",
+  title: "Anirudh Ravichander - Hayyoda",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Hayyoda.jpg"
 }
 ];
 
