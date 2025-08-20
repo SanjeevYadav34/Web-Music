0c9be3a59bca4bb417ae67a96c7ef12cd79cafe0
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-20 17:37:12 +0530

    Update script.js

--- Shuba/script.js
+++ Shuba/script.js
@@ -10,130 +10,64 @@ const loopBtn = document.getElementById("loop");
 
 const songs = [
 {
-  name: "Imagine_Dragons_-_Bones.mp3",
-  title: "Imagine Dragons - Bones",
+  name: "Anirudh_Ravichander_-_Why_This_Kolaveri_Di.mp3",
+  title: "Anirudh Ravichander - Why This Kolaveri Di",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Bones.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Why_This_Kolaveri_Di.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Bad_Liar.mp3",
-  title: "Imagine Dragons - Bad Liar",
+  name: "Anirudh_Ravichander_-_Po_Nee_Po.mp3",
+  title: "Anirudh Ravichander - Po Nee Po",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Bad_Liar.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Po_Nee_Po.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Demons.mp3",
-  title: "Imagine Dragons - Demons",
+  name: "Anirudh_Ravichander_-_Idhazhin_Oram.mp3",
+  title: "Anirudh Ravichander - Idhazhin Oram",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Demons.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Idhazhin_Oram.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Believer.mp3",
-  title: "Imagine Dragons - Believer",
+  name: "Anirudh_Ravichander_-_Velicha_Poove_Vaa.mp3",
+  title: "Anirudh Ravichander - Velicha Poove Vaa",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Believer.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Velicha_Poove_Vaa.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Enemy.mp3",
-  title: "Imagine Dragons - Enemy",
+  name: "Anirudh_Ravichander_-_Ethir_Neechal.mp3",
+  title: "Anirudh Ravichander - Ethir Neechal",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Enemy.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Ethir_Neechal.jpg"
 },
 {
-  name: "Imagine_Dragons_-_Thunder.mp3",
-  title: "Imagine Dragons - Thunder",
+  name: "Anirudh_Ravichander_-_Vanakkam_Chennai.mp3",
+  title: "Anirudh Ravichander - Vanakkam Chennai",
   artist: "Artist: Unknown",
-  cover: "covers/Imagine_Dragons_-_Thunder.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Vanakkam_Chennai.jpg"
 },
 {
-  name: "Ed_Sheeran_-_Perfect.mp3",
-  title: "Ed Sheeran - Perfect",
+  name: "Anirudh_Ravichander_-_Oh_Penne.mp3",
+  title: "Anirudh Ravichander - Oh Penne",
   artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Perfect.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Oh_Penne.jpg"
 },
 {
-  name: "Ed_Sheeran_-_Photograph.mp3",
-  title: "Ed Sheeran - Photograph",
+  name: "Anirudh_Ravichander_-_Selfie_Pulla.mp3",
+  title: "Anirudh Ravichander - Selfie Pulla",
   artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Photograph.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Selfie_Pulla.jpg"
 },
 {
-  name: "Ed_Sheeran_-_Shape_of_You.mp3",
-  title: "Ed Sheeran - Shape of You",
+  name: "Anirudh_Ravichander_-_Aaluma_Doluma.mp3",
+  title: "Anirudh Ravichander - Aaluma Doluma",
   artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Shape_of_You.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Aaluma_Doluma.jpg"
 },
 {
-  name: "Ed_-_Sheeran_-_I_Don't_Care.mp3",
-  title: "Ed - Sheeran - I Don't Care",
+  name: "Anirudh_Ravichander_-_Thangamey.mp3",
+  title: "Anirudh Ravichander - Thangamey",
   artist: "Artist: Unknown",
-  cover: "covers/Ed_-_Sheeran_-_I_Don't_Care.jpg"
-},
-{
-  name: "Ed_Sheeran_-_I_Don't_Care_Ed_Sheeran_-_Beautiful_People.mp3",
-  title: "Ed Sheeran - Beautiful People",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_I_Don't_Care_Ed_Sheeran_-_Beautiful_People.jpg"
-},
-{
-  name: "Ed_Sheeran_-_Thinking_Out_Loud.mp3",
-  title: "Ed Sheeran - Thinking Out Loud",
-  artist: "Artist: Unknown",
-  cover: "covers/Ed_Sheeran_-_Thinking_Out_Loud.jpg"
-},
-{
-  name: "Passenger_-_Let_Her_Go.mp3",
-  title: "Passenger - Let Her Go",
-  artist: "Artist: Unknown",
-  cover: "covers/Passenger_-_Let_Her_Go.jpg"
-},
-{
-  name: "Bruno_Mars_-_Talking_To_The_Moon.mp3",
-  title: "Bruno Mars - Talking To The Moon",
-  artist: "Artist: Unknown",
-  cover: "covers/Bruno_Mars_-_Talking_To_The_Moon.jpg"
-},
-{
-  name: "Marron_5_-_Girls_Like_You.mp3",
-  title: "Marron 5 - Girls Like You",
-  artist: "Artist: Unknown",
-  cover: "covers/Marron_5_-_Girls_Like_You.jpg"
-},
-{
-  name: "Post_Malone_-_Sunflower.mp3",
-  title: "Post Malone - Sunflower",
-  artist: "Artist: Unknown",
-  cover: "covers/Post_Malone_-_Sunflower.jpg"
-},
-{
-  name: "Avicii_-_The_Nights.mp3",
-  title: "Avicii - The Nights",
-  artist: "Artist: Unknown",
-  cover: "covers/Avicii_-_The_Nights.jpg"
-},
-{
-  name: "Avicii_-_Waiting_For_Love.mp3",
-  title: "Avicii - Waiting For Love",
-  artist: "Artist: Unknown",
-  cover: "covers/Avicii_-_Waiting_For_Love.jpg"
-},
-{
-  name: "The_Chainsmokers_-_Closer.mp3",
-  title: "The Chainsmokers - Closer",
-  artist: "Artist: Unknown",
-  cover: "covers/The_Chainsmokers_-_Closer.jpg"
-},
-{
-  name: "Charlie_Puth_-_One_Call_Away.mp3",
-  title: "Charlie Puth - One Call Away",
-  artist: "Artist: Unknown",
-  cover: "covers/Charlie_Puth_-_One_Call_Away.jpg"
-},
-{
-  name: "Charlie_Puth_-_We_Don't_Talk_Anymore.mp3",
-  title: "Charlie Puth - We Don't Talk Anymore",
-  artist: "Artist: Unknown",
-  cover: "covers/Charlie_Puth_-_We_Don't_Talk_Anymore.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Thangamey.jpg"
 }
 ];
 
