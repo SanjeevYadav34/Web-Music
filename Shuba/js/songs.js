c97bab216e2d8ffa6a0c18e4ea081faeb5cb1262
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-08-21 05:01:55 +0530

    added

--- Shuba/js/songs.js
+++ Shuba/js/songs.js
@@ -1,122 +1,122 @@
 const songs = [
 {
-  name: "Anirudh_Ravichander_-_Kaaki_Sattai_Title_Track.mp3",
-  title: "Anirudh Ravichander - Kaaki Sattai Title Track",
+  name: "Anirudh_Ravichander_-_Damaalu_Dumeelu.mp3",
+  title: "Anirudh Ravichander - Damaalu Dumeelu",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Kaaki_Sattai_Title_Track.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Damaalu_Dumeelu.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Maari_Thara_Local.mp3",
-  title: "Anirudh Ravichander - Maari Thara Local",
+  name: "Anirudh_Ravichander_-_Rangu_Rakkara.mp3",
+  title: "Anirudh Ravichander - Rangu Rakkara",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Maari_Thara_Local.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Rangu_Rakkara.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Donu_Donu_Donu.mp3",
-  title: "Anirudh Ravichander - Donu Donu Donu",
+  name: "Anirudh_Ravichander_-_Ra_Ra_Ra.mp3",
+  title: "Anirudh Ravichander - Ra Ra Ra",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Donu_Donu_Donu.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Ra_Ra_Ra.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Yennai_Maatrum_Kadhale.mp3",
-  title: "Anirudh Ravichander - Yennai Maatrum Kadhale",
+  name: "Anirudh_Ravichander_-_Karukku_kallangolu.mp3",
+  title: "Anirudh Ravichander - Karukku kallangolu",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Yennai_Maatrum_Kadhale.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Karukku_kallangolu.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Varavaa_Varavaa.mp3",
-  title: "Anirudh Ravichander - Varavaa Varavaa",
+  name: "Anirudh_Ravichander_-_Hai,_En_Kai_Mela.mp3",
+  title: "Anirudh Ravichander - Hai, En Kai Mela",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Varavaa_Varavaa.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Hai,_En_Kai_Mela.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_The_Theri_Theme_Song.mp3",
-  title: "Anirudh Ravichander - The Theri Theme Song",
+  name: "Anirudh_Ravichander_-_Foreign_Return.mp3",
+  title: "Anirudh Ravichander - Foreign Return",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_The_Theri_Theme_Song.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Foreign_Return.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Veera_Vinayaka.mp3",
-  title: "Anirudh Ravichander - Veera Vinayaka",
+  name: "Anirudh_Ravichander_-_Yaanji.mp3",
+  title: "Anirudh Ravichander - Yaanji",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Veera_Vinayaka.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Yaanji.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Tak_Bak.mp3",
-  title: "Anirudh Ravichander - Tak Bak",
+  name: "Anirudh_Ravichander_-_On_Nenappu.mp3",
+  title: "Anirudh Ravichander - On Nenappu",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Tak_Bak.jpg"
+  cover: "covers/Anirudh_Ravichander_-_On_Nenappu.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Marandhadhae.mp3",
-  title: "Anirudh Ravichander - Marandhadhae",
+  name: "Anirudh_Ravichander_-_Karuthavanlaam_Galeejaam.mp3",
+  title: "Anirudh Ravichander - Karuthavanlaam Galeejaam",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Marandhadhae.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Karuthavanlaam_Galeejaam.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Theeramal.mp3",
-  title: "Anirudh Ravichander - Theeramal",
+  name: "Anirudh_Ravichander_-_Iraiva.mp3",
+  title: "Anirudh Ravichander - Iraiva",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Theeramal.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Iraiva.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Naanga_Podiyan.mp3",
-  title: "Anirudh Ravichander - Naanga Podiyan",
+  name: "Anirudh_Ravichander_-_Idhayane.mp3",
+  title: "Anirudh Ravichander - Idhayane",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Naanga_Podiyan.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Idhayane.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Shoot_The_Kuruvi.mp3",
-  title: "Anirudh Ravichander - Shoot The Kuruvi",
+  name: "Anirudh_Ravichander_-_Othaiyadi_Pathayila.mp3",
+  title: "Anirudh Ravichander - Othaiyadi Pathayila",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Shoot_The_Kuruvi.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Othaiyadi_Pathayila.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Hey_Mama.mp3",
-  title: "Anirudh Ravichander - Hey Mama",
+  name: "Anirudh_Ravichander_-_Kannampoochi.mp3",
+  title: "Anirudh Ravichander - Kannampoochi",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Hey_Mama.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Kannampoochi.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Akka_Petha_Jakkavandi.mp3",
-  title: "Anirudh Ravichander - Akka Petha Jakkavandi",
+  name: "Anirudh_Ravichander_-_Ezhu_Ezhu.mp3",
+  title: "Anirudh Ravichander - Ezhu Ezhu",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Akka_Petha_Jakkavandi.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Ezhu_Ezhu.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Remo_Nee_Kadhalan.mp3",
-  title: "Anirudh Ravichander - Remo Nee Kadhalan",
+  name: "Anirudh_Ravichander_-_Ilamai_Thirumbudhe.mp3",
+  title: "Anirudh Ravichander - Ilamai Thirumbudhe",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Remo_Nee_Kadhalan.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Ilamai_Thirumbudhe.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Senjitaley.mp3",
-  title: "Anirudh Ravichander - Senjitaley",
+  name: "Anirudh_Ravichander_-_Marana_Mass.mp3",
+  title: "Anirudh Ravichander - Marana Mass",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Senjitaley.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Marana_Mass.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Meesa_Beauty.mp3",
-  title: "Anirudh Ravichander - Meesa Beauty",
+  name: "Anirudh_Ravichander_-_Petta_Paraak.mp3",
+  title: "Anirudh Ravichander - Petta Paraak",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Meesa_Beauty.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Petta_Paraak.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Tamilselvi.mp3",
-  title: "Anirudh Ravichander - Tamilselvi",
+  name: "Anirudh_Ravichander_-_Kannamma.mp3",
+  title: "Anirudh Ravichander - Kannamma",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Tamilselvi.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Kannamma.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Sirikkadhey.mp3",
-  title: "Anirudh Ravichander - Sirikkadhey",
+  name: "Anirudh_Ravichander_-_Marakkavillayae.mp3",
+  title: "Anirudh Ravichander - Marakkavillayae",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Sirikkadhey.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Marakkavillayae.jpg"
 },
 {
-  name: "Anirudh_Ravichander_-_Ondroduthan_Ondroga.mp3",
-  title: "Anirudh Ravichander - Ondroduthan Ondroga",
+  name: "Anirudh_Ravichander_-_Takkunu_Takkunu.mp3",
+  title: "Anirudh Ravichander - Takkunu Takkunu",
   artist: "Artist: Unknown",
-  cover: "covers/Anirudh_Ravichander_-_Ondroduthan_Ondroga.jpg"
+  cover: "covers/Anirudh_Ravichander_-_Takkunu_Takkunu.jpg"
 }
 ];
