3a590dec9e68d48ae8b4ee42b224cdbd1e170100
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-20 17:50:43 +0530

    Update script.js

--- Shuba/script.js
+++ Shuba/script.js
@@ -68,6 +68,66 @@ const songs = [
   title: "Anirudh Ravichander - Thangamey",
   artist: "Artist: Unknown",
   cover: "covers/Anirudh_Ravichander_-_Thangamey.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Azhage_Azhage.mp3",
+  title: "Anirudh Ravichander - Azhage Azhage",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Azhage_Azhage.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Surviva.mp3",
+  title: "Anirudh Ravichander - Surviva",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Surviva.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Vivegam_Theme.mp3",
+  title: "Anirudh Ravichander - Vivegam Theme",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Vivegam_Theme.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Sodakku.mp3",
+  title: "Anirudh Ravichander - Sodakku",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Sodakku.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Gaali_Vaaluga.mp3",
+  title: "Anirudh Ravichander - Gaali Vaaluga",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Gaali_Vaaluga.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Kutti_Story.mp3",
+  title: "Anirudh Ravichander - Kutti Story",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Kutti_Story.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Vaathi_Coming.mp3",
+  title: "Anirudh Ravichander - Vaathi Coming",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Vaathi_Coming.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Master_the_Blaster.mp3",
+  title: "Anirudh Ravichander - Master the Blaster",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Master_the_Blaster.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Chellamma.mp3",
+  title: "Anirudh Ravichander - Chellamma",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Chellamma.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Arabic_Kuthu_(Halamithi_Habibo).mp3",
+  title: "Anirudh Ravichander - Arabic Kuthu (Halamithi Habibo)",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Arabic_Kuthu_(Halamithi_Habibo).jpg"
 }
 ];
 
