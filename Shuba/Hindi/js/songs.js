f3a4ec9f603073c9c2abd31f1dbea6e0475d7233
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-08-20 17:33:50 +0530

    shuba

---
+++ Shuba/Hindi/js/songs.js
@@ -0,0 +1,8 @@
+const songs = [
+{
+  name: "Amit_Trivedi_-_Naina_Da_Kya_Kasoor.mp3",
+  title: "Amit Trivedi - Naina Da Kya Kasoor",
+  artist: "Artist: Unknown",
+  cover: "covers/Amit_Trivedi_-_Naina_Da_Kya_Kasoor.jpg"
+}
+];
---
+++ Shuba/js/songs.js
@@ -0,0 +1,62 @@
+const songs = [
+{
+  name: "Anirudh_Ravichander_-_Why_This_Kolaveri_Di.mp3",
+  title: "Anirudh Ravichander - Why This Kolaveri Di",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Why_This_Kolaveri_Di.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Po_Nee_Po.mp3",
+  title: "Anirudh Ravichander - Po Nee Po",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Po_Nee_Po.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Idhazhin_Oram.mp3",
+  title: "Anirudh Ravichander - Idhazhin Oram",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Idhazhin_Oram.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Velicha_Poove_Vaa.mp3",
+  title: "Anirudh Ravichander - Velicha Poove Vaa",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Velicha_Poove_Vaa.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Ethir_Neechal.mp3",
+  title: "Anirudh Ravichander - Ethir Neechal",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Ethir_Neechal.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Vanakkam_Chennai.mp3",
+  title: "Anirudh Ravichander - Vanakkam Chennai",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Vanakkam_Chennai.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Oh_Penne.mp3",
+  title: "Anirudh Ravichander - Oh Penne",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Oh_Penne.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Selfie_Pulla.mp3",
+  title: "Anirudh Ravichander - Selfie Pulla",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Selfie_Pulla.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Aaluma_Doluma.mp3",
+  title: "Anirudh Ravichander - Aaluma Doluma",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Aaluma_Doluma.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Thangamey.mp3",
+  title: "Anirudh Ravichander - Thangamey",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Thangamey.jpg"
+}
+];
