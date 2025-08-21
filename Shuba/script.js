75ab67f1df4334f798cd7c5e4b6b76189911d12e
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-21 06:11:20 +0530

    Update script.js

--- Shuba/script.js
+++ Shuba/script.js
@@ -614,6 +614,126 @@ const songs = [
   title: "Anirudh Ravichander - Rendu Kaadhal",
   artist: "Artist: Unknown",
   cover: "covers/Anirudh_Ravichander_-_Rendu_Kaadhal.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Two_Two_Two.mp3",
+  title: "Anirudh Ravichander - Two Two Two",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Two_Two_Two.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Jalabulajangu.mp3",
+  title: "Anirudh Ravichander - Jalabulajangu",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Jalabulajangu.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Once_Upon_A_Time.mp3",
+  title: "Anirudh Ravichander - Once Upon A Time",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Once_Upon_A_Time.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Porkanda_Singam.mp3",
+  title: "Anirudh Ravichander - Porkanda Singam",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Porkanda_Singam.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Vikram_Title_Song.mp3",
+  title: "Anirudh Ravichander - Vikram Title Song",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Vikram_Title_Song.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Wasted.mp3",
+  title: "Anirudh Ravichander - Wasted",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Wasted.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Hukum_–_Thalaivar_Alappara.mp3",
+  title: "Anirudh Ravichander - Hukum – Thalaivar Alappara",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Hukum_–_Thalaivar_Alappara.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Anbenum.mp3",
+  title: "Anirudh Ravichander - Anbenum",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Anbenum.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Badass.mp3",
+  title: "Anirudh Ravichander - Badass",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Badass.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Bloody_Sweet.mp3",
+  title: "Anirudh Ravichander - Bloody Sweet",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Bloody_Sweet.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_I’m_Scared.mp3",
+  title: "Anirudh Ravichander - I’m Scared",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_I’m_Scared.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Lokiverse_2.0.mp3",
+  title: "Anirudh Ravichander - Lokiverse 2.0",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Lokiverse_2.0.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Villain_Yaaru.mp3",
+  title: "Anirudh Ravichander - Villain Yaaru",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Villain_Yaaru.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Come_Back_Indian.mp3",
+  title: "Anirudh Ravichander - Come Back Indian",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Come_Back_Indian.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Kadharalz.mp3",
+  title: "Anirudh Ravichander - Kadharalz",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Kadharalz.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Paaraa.mp3",
+  title: "Anirudh Ravichander - Paaraa",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Paaraa.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Fear_Song.mp3",
+  title: "Anirudh Ravichander - Fear Song",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Fear_Song.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Manasilaayo.mp3",
+  title: "Anirudh Ravichander - Manasilaayo",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Manasilaayo.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Chuttamalle.mp3",
+  title: "Anirudh Ravichander - Chuttamalle",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Chuttamalle.jpg"
+},
+{
+  name: "Anirudh_Ravichander_-_Kaavaalaa.mp3",
+  title: "Anirudh Ravichander - Kaavaalaa",
+  artist: "Artist: Unknown",
+  cover: "covers/Anirudh_Ravichander_-_Kaavaalaa.jpg"
 }
 ];
 
