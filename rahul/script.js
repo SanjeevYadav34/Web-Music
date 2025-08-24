414105d71b076f0fc62e4ebfba5aa59d1f838f91
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-24 22:07:38 +0530

    Update script.js

--- rahul/script.js
+++ rahul/script.js
@@ -752,6 +752,102 @@ const songs = [
   title: "Biba Sada (From Abdullahpur",
   artist: "Artist: Unknown",
   cover: "covers/Biba_Sada_(From_Abdullahpur_Ka_Devdas).jpg"
+},
+{
+  name: "Vaaroon.mp3",
+  title: "Vaaroon",
+  artist: "Artist: Unknown",
+  cover: "covers/Vaaroon.jpg"
+},
+{
+  name: "Chinnamma_Chilakkamma.mp3",
+  title: "Chinnamma Chilakkamma",
+  artist: "Artist: Unknown",
+  cover: "covers/Chinnamma_Chilakkamma.jpg"
+},
+{
+  name: "Katchi_Sera_-_Sai_Abhyankkar_|_Samyuktha_|_Ken_Royson.mp3",
+  title: "Katchi Sera - Sai Abhyankkar | Samyuktha | Ken Royson",
+  artist: "Artist: Unknown",
+  cover: "covers/Katchi_Sera_-_Sai_Abhyankkar_|_Samyuktha_|_Ken_Royson.jpg"
+},
+{
+  name: "Afghan_Jalebi_(Ya_Baba)_-_Phantom_|_Saif_Ali_Khan,_Katrina_Kaif.mp3",
+  title: "Afghan Jalebi (Ya Baba) - Phantom | Saif Ali Khan, Katrina Kaif",
+  artist: "Artist: Unknown",
+  cover: "covers/Afghan_Jalebi_(Ya_Baba)_-_Phantom_|_Saif_Ali_Khan,_Katrina_Kaif.jpg"
+},
+{
+  name: "Keejo_Kesari_Ke_Laal_-_Jai_Shree_Ram_|_DJ_Karan_Kahar.mp3",
+  title: "Keejo Kesari Ke Laal - Jai Shree Ram | DJ Karan Kahar",
+  artist: "Artist: Unknown",
+  cover: "covers/Keejo_Kesari_Ke_Laal_-_Jai_Shree_Ram_|_DJ_Karan_Kahar.jpg"
+},
+{
+  name: "Natin_Marli_Mithi_-_DJ_Mix_Song_2025.mp3",
+  title: "Natin Marli Mithi - DJ Mix Song 2025",
+  artist: "Artist: Unknown",
+  cover: "covers/Natin_Marli_Mithi_-_DJ_Mix_Song_2025.jpg"
+},
+{
+  name: "Dekh_Tuni_Bayko_-_Ahirani_Song_|_Anjana_Barlekar.mp3",
+  title: "Dekh Tuni Bayko - Ahirani Song | Anjana Barlekar",
+  artist: "Artist: Unknown",
+  cover: "covers/Dekh_Tuni_Bayko_-_Ahirani_Song_|_Anjana_Barlekar.jpg"
+},
+{
+  name: "Zingaat_-_Sairat_|_Ajay_Atul_|_Akash_Thosar,_Rinku_Rajguru.mp3",
+  title: "Zingaat - Sairat | Ajay Atul | Akash Thosar, Rinku Rajguru",
+  artist: "Artist: Unknown",
+  cover: "covers/Zingaat_-_Sairat_|_Ajay_Atul_|_Akash_Thosar,_Rinku_Rajguru.jpg"
+},
+{
+  name: "Mala_Pirtichya_Jhulyat_Jhulwa_-_Lavni_Song.mp3",
+  title: "Mala Pirtichya Jhulyat Jhulwa - Lavni Song",
+  artist: "Artist: Unknown",
+  cover: "covers/Mala_Pirtichya_Jhulyat_Jhulwa_-_Lavni_Song.jpg"
+},
+{
+  name: "Patlancha_Bailgada_-_Radha_Khude_|_Swapnil_Gaikwad.mp3",
+  title: "Patlancha Bailgada - Radha Khude | Swapnil Gaikwad",
+  artist: "Artist: Unknown",
+  cover: "covers/Patlancha_Bailgada_-_Radha_Khude_|_Swapnil_Gaikwad.jpg"
+},
+{
+  name: "Mehbooba_-_Preet_Bandre.mp3",
+  title: "Mehbooba - Preet Bandre",
+  artist: "Artist: Unknown",
+  cover: "covers/Mehbooba_-_Preet_Bandre.jpg"
+},
+{
+  name: "Aai_Tuza_Dongar_-_Ekveera_Aai_Song_|_Amol_Jadhav.mp3",
+  title: "Aai Tuza Dongar - Ekveera Aai Song | Amol Jadhav",
+  artist: "Artist: Unknown",
+  cover: "covers/Aai_Tuza_Dongar_-_Ekveera_Aai_Song_|_Amol_Jadhav.jpg"
+},
+{
+  name: "Tuzya_Priticha_Vinchu_Chawla_-_Fandry_|_Ajay_Atul.mp3",
+  title: "Tuzya Priticha Vinchu Chawla - Fandry | Ajay Atul",
+  artist: "Artist: Unknown",
+  cover: "covers/Tuzya_Priticha_Vinchu_Chawla_-_Fandry_|_Ajay_Atul.jpg"
+},
+{
+  name: "Abhang_-_Kanada_Raja_Pandharicha.mp3",
+  title: "Abhang - Kanada Raja Pandharicha",
+  artist: "Artist: Unknown",
+  cover: "covers/Abhang_-_Kanada_Raja_Pandharicha.jpg"
+},
+{
+  name: "Jagnayache_Deva_-_Swapnil_Bandodkar_|_Dashakriya.mp3",
+  title: "Jagnayache Deva - Swapnil Bandodkar | Dashakriya",
+  artist: "Artist: Unknown",
+  cover: "covers/Jagnayache_Deva_-_Swapnil_Bandodkar_|_Dashakriya.jpg"
+},
+{
+  name: "Damlelya_Babachi_Kahani.mp3",
+  title: "Damlelya Babachi Kahani",
+  artist: "Artist: Unknown",
+  cover: "covers/Damlelya_Babachi_Kahani.jpg"
 }
 ];
 
