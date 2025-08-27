13b9a2766ff33cdac7418d5636a06ec3d11c8dbf
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-08-27 06:05:18 +0530

    Update script.js

--- Indrajit/Kishore_kumar/script.js
+++ Indrajit/Kishore_kumar/script.js
@@ -128,6 +128,54 @@ const songs = [
   title: "Kishore Kumar - Chalte Chalte Mere Yeh Geet Yaad Rakhna",
   artist: "Artist: Unknown",
   cover: "covers/Kishore_Kumar_-_Chalte_Chalte_Mere_Yeh_Geet_Yaad_Rakhna.jpg"
+},
+{
+  name: "Kishore_Kumar_-_Yeh_Jo_Mohabbat_Hai.mp3",
+  title: "Kishore Kumar - Yeh Jo Mohabbat Hai",
+  artist: "Artist: Unknown",
+  cover: "covers/Kishore_Kumar_-_Yeh_Jo_Mohabbat_Hai.jpg"
+},
+{
+  name: "Kishore_Kumar_-_Ek_Ladki_Bheegi_Bhagi_Si.mp3",
+  title: "Kishore Kumar - Ek Ladki Bheegi Bhagi Si",
+  artist: "Artist: Unknown",
+  cover: "covers/Kishore_Kumar_-_Ek_Ladki_Bheegi_Bhagi_Si.jpg"
+},
+{
+  name: "Kishore_Kumar_-_Diye_Jalte_Hai_Phool_Khilte_Hai.mp3",
+  title: "Kishore Kumar - Diye Jalte Hai Phool Khilte Hai",
+  artist: "Artist: Unknown",
+  cover: "covers/Kishore_Kumar_-_Diye_Jalte_Hai_Phool_Khilte_Hai.jpg"
+},
+{
+  name: "Kishore_Kumar_-_Mere_Mehboob_Qayamat_Hogi.mp3",
+  title: "Kishore Kumar - Mere Mehboob Qayamat Hogi",
+  artist: "Artist: Unknown",
+  cover: "covers/Kishore_Kumar_-_Mere_Mehboob_Qayamat_Hogi.jpg"
+},
+{
+  name: "Kishore_Kumar_-_Apni_To_Jaise_Taise_Kat_Jayegi.mp3",
+  title: "Kishore Kumar - Apni To Jaise Taise Kat Jayegi",
+  artist: "Artist: Unknown",
+  cover: "covers/Kishore_Kumar_-_Apni_To_Jaise_Taise_Kat_Jayegi.jpg"
+},
+{
+  name: "Kishore_Kumar_-_Mere_Naina_Sawan_Bhadon_Phir_Bhi_Mera_Man_Pyasa.mp3",
+  title: "Kishore Kumar - Mere Naina Sawan Bhadon Phir Bhi Mera Man Pyasa",
+  artist: "Artist: Unknown",
+  cover: "covers/Kishore_Kumar_-_Mere_Naina_Sawan_Bhadon_Phir_Bhi_Mera_Man_Pyasa.jpg"
+},
+{
+  name: "Kishore_Kumar_-_Mere_Samne_Wali_Khidki_Mein.mp3",
+  title: "Kishore Kumar - Mere Samne Wali Khidki Mein",
+  artist: "Artist: Unknown",
+  cover: "covers/Kishore_Kumar_-_Mere_Samne_Wali_Khidki_Mein.jpg"
+},
+{
+  name: "Kishore_Kumar_-_Chalte_Chalte.mp3",
+  title: "Kishore Kumar - Chalte Chalte",
+  artist: "Artist: Unknown",
+  cover: "covers/Kishore_Kumar_-_Chalte_Chalte.jpg"
 }
 ];
 
