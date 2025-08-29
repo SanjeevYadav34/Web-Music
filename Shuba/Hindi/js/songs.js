40fa1fd6cebe00523f93908b7ed916565506645e
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-08-30 04:58:36 +0530

    Added

--- Shuba/Hindi/js/songs.js
+++ Shuba/Hindi/js/songs.js
@@ -1,8 +1,92 @@
 const songs = [
 {
-  name: "Amit_Trivedi_-_Naina_Da_Kya_Kasoor.mp3",
-  title: "Amit Trivedi - Naina Da Kya Kasoor",
+  name: "Sai_Abhyankkar_-_Valam_Varavendum.mp3",
+  title: "Sai Abhyankkar - Valam Varavendum",
   artist: "Artist: Unknown",
-  cover: "covers/Amit_Trivedi_-_Naina_Da_Kya_Kasoor.jpg"
+  cover: "covers/Sai_Abhyankkar_-_Valam_Varavendum.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Aasa_Kooda.mp3",
+  title: "Sai Abhyankkar - Aasa Kooda",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Aasa_Kooda.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Sithira_Puthiri.mp3",
+  title: "Sai Abhyankkar - Sithira Puthiri",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Sithira_Puthiri.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Vizhi_Veekura.mp3",
+  title: "Sai Abhyankkar - Vizhi Veekura",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Vizhi_Veekura.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Salambala.mp3",
+  title: "Sai Abhyankkar - Salambala",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Salambala.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Verappa.mp3",
+  title: "Sai Abhyankkar - Verappa",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Verappa.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Benz_In_The_Universe_Theme.mp3",
+  title: "Sai Abhyankkar - Benz In The Universe Theme",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Benz_In_The_Universe_Theme.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Twin_Fish_Walter.mp3",
+  title: "Sai Abhyankkar - Twin Fish Walter",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Twin_Fish_Walter.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Blud_Is_On_His_Way.mp3",
+  title: "Sai Abhyankkar - Blud Is On His Way",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Blud_Is_On_His_Way.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Soda_Break.mp3",
+  title: "Sai Abhyankkar - Soda Break",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Soda_Break.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Boom_Boom_(From_Dude_-_Telugu).mp3",
+  title: "Sai Abhyankkar - Boom Boom (From Dude - Telugu)",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Boom_Boom_(From_Dude_-_Telugu).jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Oorum_Blood.mp3",
+  title: "Sai Abhyankkar - Oorum Blood",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Oorum_Blood.jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Salambala_(From_Madharaasi).mp3",
+  title: "Sai Abhyankkar - Salambala (From Madharaasi)",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Salambala_(From_Madharaasi).jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Jaalakaari_(From_Balti).mp3",
+  title: "Sai Abhyankkar - Jaalakaari (From Balti)",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Jaalakaari_(From_Balti).jpg"
+},
+{
+  name: "Sai_Abhyankkar_-_Boom_Boom_(From_Dude_-_Telugu,_Film_Version).mp3",
+  title: "Sai Abhyankkar - Boom Boom (From Dude - Telugu, Film Version)",
+  artist: "Artist: Unknown",
+  cover: "covers/Sai_Abhyankkar_-_Boom_Boom_(From_Dude_-_Telugu,_Film_Version).jpg"
 }
 ];
