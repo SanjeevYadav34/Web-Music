46bc3f598bb553e6ec9e176a0f1b909ad5d8bc17
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-09-11 18:18:21 +0530

    Update script.js

--- Shuba/hip_hop_tamizha/script.js
+++ Shuba/hip_hop_tamizha/script.js
@@ -248,6 +248,120 @@ const songs = [
   title: "Hiphop Tamizha - Inayam",
   artist: "Artist: Unknown",
   cover: "covers/Hiphop_Tamizha_-_Inayam.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Pogattum_Pogattum_Po.mp3",
+  title: "Pogattum Pogattum Po",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Pogattum_Pogattum_Po.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Yaarumey_Venam_alternate_listing_single.mp3",
+  title: "Yaarumey Venam (alternate listing / single)",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Yaarumey_Venam_alternate_listing_single.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Amigo.mp3",
+  title: "Amigo",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Amigo.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Veedhikoka_Jaathi.mp3",
+  title: "Veedhikoka Jaathi",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Veedhikoka_Jaathi.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Telavaarutunte.mp3",
+  title: "Telavaarutunte",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Telavaarutunte.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Seatu_Siragadha.mp3",
+  title: "Seatu Siragadha",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Seatu_Siragadha.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Puttibhoomi.mp3",
+  title: "Puttibhoomi",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Puttibhoomi.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Poratamey.mp3",
+  title: "Poratamey",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Poratamey.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Charitraney_Likhinchara.mp3",
+  title: "Charitraney Likhinchara",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Charitraney_Likhinchara.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Sivakumar_Pondati.mp3",
+  title: "Sivakumar Pondati",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Sivakumar_Pondati.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Bahubaliku_Oru_Kattappa.mp3",
+  title: "Bahubaliku Oru Kattappa",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Bahubaliku_Oru_Kattappa.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Neruppa_Irupaan.mp3",
+  title: "Neruppa Irupaan",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Neruppa_Irupaan.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Thillalangadi_Lady.mp3",
+  title: "Thillalangadi Lady",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Thillalangadi_Lady.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_BreakUp_Song_alternate_listings.mp3",
+  title: "BreakUp Song (alternate listings)",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_BreakUp_Song_alternate_listings.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Thappellam_Thappe_Illai_from_Naan.mp3",
+  title: "Thappellam Thappe Illai (from Naan)",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Thappellam_Thappe_Illai_from_Naan.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Thani_Oruvan_theme_song.mp3",
+  title: "Thani Oruvan (theme / song)",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Thani_Oruvan_theme_song.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Theemai_Dhaan_Vellum.mp3",
+  title: "Theemai Dhaan Vellum",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Theemai_Dhaan_Vellum.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Aasai_Peraasai.mp3",
+  title: "Aasai Peraasai",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Aasai_Peraasai.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Pazhagikalaam_Aambala.mp3",
+  title: "Pazhagikalaam (Aambala)",
+  artist: "Artist: Hiphop Tamizha",
+  cover: "covers/Hiphop_Tamizha_-_Pazhagikalaam_Aambala.webp"
 }
 ];
 
