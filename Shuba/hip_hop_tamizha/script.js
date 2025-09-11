2e76ac58c2bce1a1372b34936718858d71687970
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-09-11 17:59:47 +0530

    Update script.js

--- Shuba/hip_hop_tamizha/script.js
+++ Shuba/hip_hop_tamizha/script.js
@@ -128,6 +128,126 @@ const songs = [
   title: "Hiphop Tamizha - Maanavan",
   artist: "Artist: Hiphop Tamizha",
   cover: "covers/Hiphop_Tamizha_-_Maanavan.jpg"
+},
+{
+  name: "Hiphop_Tamizha_-_Tamizhi_(single).mp3",
+  title: "Hiphop Tamizha - Tamizhi (single)",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Tamizhi_(single).webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Quarantine_&_Chill_(single).mp3",
+  title: "Hiphop Tamizha - Quarantine & Chill (single)",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Quarantine_&_Chill_(single).webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Oorukaaran_(single).mp3",
+  title: "Hiphop Tamizha - Oorukaaran (single)",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Oorukaaran_(single).webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Chinna_Paiyan.mp3",
+  title: "Hiphop Tamizha - Chinna Paiyan",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Chinna_Paiyan.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Nadanthavaraikumey.mp3",
+  title: "Hiphop Tamizha - Nadanthavaraikumey",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Nadanthavaraikumey.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Poi_Poi_Poi.mp3",
+  title: "Hiphop Tamizha - Poi Poi Poi",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Poi_Poi_Poi.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_International_(single).mp3",
+  title: "Hiphop Tamizha - International (single)",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_International_(single).webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Single_Kingulam_(A1_Express).mp3",
+  title: "Hiphop Tamizha - Single Kingulam (A1 Express)",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Single_Kingulam_(A1_Express).webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Net_ah_Thorandha_(Naa_Oru_Alien).mp3",
+  title: "Hiphop Tamizha - Net ah Thorandha (Naa Oru Alien)",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Net_ah_Thorandha_(Naa_Oru_Alien).webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Yaarumey_Venam.mp3",
+  title: "Hiphop Tamizha - Yaarumey Venam",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Yaarumey_Venam.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_BreakUp_Song_(Naan_Sirithal).mp3",
+  title: "Hiphop Tamizha - BreakUp Song (Naan Sirithal)",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_BreakUp_Song_(Naan_Sirithal).webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Dhom_Dhom.mp3",
+  title: "Hiphop Tamizha - Dhom Dhom",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Dhom_Dhom.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Ajukku_Gumukku.mp3",
+  title: "Hiphop Tamizha - Ajukku Gumukku",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Ajukku_Gumukku.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Happy_Birthday.mp3",
+  title: "Hiphop Tamizha - Happy Birthday",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Happy_Birthday.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Keka_Beka.mp3",
+  title: "Hiphop Tamizha - Keka Beka",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Keka_Beka.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Naan_Siricha.mp3",
+  title: "Hiphop Tamizha - Naan Siricha",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Naan_Siricha.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Odavum_Mudiyathu_Oliyavum_Mudiyathu.mp3",
+  title: "Hiphop Tamizha - Odavum Mudiyathu Oliyavum Mudiyathu",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Odavum_Mudiyathu_Oliyavum_Mudiyathu.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Dark_Thoughts.mp3",
+  title: "Hiphop Tamizha - Dark Thoughts",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Dark_Thoughts.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Ellamey_Konja_Kaalam.mp3",
+  title: "Hiphop Tamizha - Ellamey Konja Kaalam",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Ellamey_Konja_Kaalam.webp"
+},
+{
+  name: "Hiphop_Tamizha_-_Inayam.mp3",
+  title: "Hiphop Tamizha - Inayam",
+  artist: "Artist: Unknown",
+  cover: "covers/Hiphop_Tamizha_-_Inayam.webp"
 }
 ];
 
