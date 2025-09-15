ad23e3dbfdc759c627affd43342a6a494f280ca3
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-09-16 05:10:46 +0530

    Update script.js

--- Hindi/script.js
+++ Hindi/script.js
@@ -16,7 +16,7 @@ const songs = [
   cover: "covers/Nazar__na_lagg_jae_|_STREE.jpg"
 },
 {
-  name: "Bismil_|_Haider.mp3",
+  name: "songs/Bismil_|_Haider.mp3",
   title: "Bismil | Haider",
   artist: "Artist: Unknown",
   cover: "covers/Bismil_|_Haider.jpg"
