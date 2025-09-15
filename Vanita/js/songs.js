e091f690dc6f03e2bbe22f21699e46629dc4070f
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-09-15 09:54:37 +0530

    Auto update

--- Vanita/js/songs.js
+++ Vanita/js/songs.js
@@ -1,80 +1,8 @@
 const songs = [
 {
-  name: "Tum_Se_Hi_Pritam_Mohit_Chauhan_Irshad_Kamil.mp3",
-  title: "Tum Se Hi Pritam Mohit Chauhan Irshad Kamil",
+  name: "Karan_Aujla_-_For_a_reason.mp3",
+  title: "Karan Aujla - For a reason",
   artist: "Artist: Unknown",
-  cover: "covers/Tum_Se_Hi_Pritam_Mohit_Chauhan_Irshad_Kamil.jpg"
-},
-{
-  name: "Dhoonde_Akhiyaan_From_Jabariya_Jodi_Yasser_Desai_Altamash_Faridi_Rashmi_Virag.mp3",
-  title: "Dhoonde Akhiyaan From Jabariya Jodi Yasser Desai Altamash Faridi Rashmi Virag",
-  artist: "Artist: Unknown",
-  cover: "covers/Dhoonde_Akhiyaan_From_Jabariya_Jodi_Yasser_Desai_Altamash_Faridi_Rashmi_Virag.jpg"
-},
-{
-  name: "Mere_Bina_Pritam_Nikhil_D'Souza.mp3",
-  title: "Mere Bina Pritam Nikhil D'Souza",
-  artist: "Artist: Unknown",
-  cover: "covers/Mere_Bina_Pritam_Nikhil_D'Souza.jpg"
-},
-{
-  name: "Moh_Moh_Ke_Dhaage_Female_Anu_Malik_Monali_Thakur_Varun_Grover.mp3",
-  title: "Moh Moh Ke Dhaage Female Anu Malik Monali Thakur Varun Grover",
-  artist: "Artist: Unknown",
-  cover: "covers/Moh_Moh_Ke_Dhaage_Female_Anu_Malik_Monali_Thakur_Varun_Grover.jpg"
-},
-{
-  name: "Tum_Mile_Pritam_Neeraj_Shridhar.mp3",
-  title: "Tum Mile Pritam Neeraj Shridhar",
-  artist: "Artist: Unknown",
-  cover: "covers/Tum_Mile_Pritam_Neeraj_Shridhar.jpg"
-},
-{
-  name: "Ajab_Si_Vishal-Shekhar_KK.mp3",
-  title: "Ajab Si Vishal-Shekhar KK",
-  artist: "Artist: Unknown",
-  cover: "covers/Ajab_Si_Vishal-Shekhar_KK.jpg"
-},
-{
-  name: "Raanjhanaa_From_Raanjhanaa_A_R_Rahman_Jaswinder_Singh.mp3",
-  title: "Raanjhanaa From Raanjhanaa A R Rahman Jaswinder Singh",
-  artist: "Artist: Unknown",
-  cover: "covers/Raanjhanaa_From_Raanjhanaa_A_R_Rahman_Jaswinder_Singh.jpg"
-},
-{
-  name: "Mera_Yaar_Shankar-Ehsaan-Loy_Javed_Bashir.mp3",
-  title: "Mera Yaar Shankar-Ehsaan-Loy Javed Bashir",
-  artist: "Artist: Unknown",
-  cover: "covers/Mera_Yaar_Shankar-Ehsaan-Loy_Javed_Bashir.jpg"
-},
-{
-  name: "Khudaya_Khair_Pritam_Soham_Chakraborty_Akriti_Kakar.mp3",
-  title: "Khudaya Khair Pritam Soham Chakraborty Akriti Kakar",
-  artist: "Artist: Unknown",
-  cover: "covers/Khudaya_Khair_Pritam_Soham_Chakraborty_Akriti_Kakar.jpg"
-},
-{
-  name: "Saudebazi_Encore_Pritam_Javed_Ali.mp3",
-  title: "Saudebazi Encore Pritam Javed Ali",
-  artist: "Artist: Unknown",
-  cover: "covers/Saudebazi_Encore_Pritam_Javed_Ali.jpg"
-},
-{
-  name: "Ramta_Jogi_Sukhwinder_Singh_Alka_Yagnik.mp3",
-  title: "Ramta Jogi Sukhwinder Singh Alka Yagnik",
-  artist: "Artist: Unknown",
-  cover: "covers/Ramta_Jogi_Sukhwinder_Singh_Alka_Yagnik.jpg"
-},
-{
-  name: "Taal_Se_Taal_Western_Version_Sukhwinder_Singh.mp3",
-  title: "Taal Se Taal Western Version Sukhwinder Singh",
-  artist: "Artist: Unknown",
-  cover: "covers/Taal_Se_Taal_Western_Version_Sukhwinder_Singh.jpg"
-},
-{
-  name: "Kehna_Hi_Kya_From_Bombay_K._S._Chithra.mp3",
-  title: "Kehna Hi Kya From Bombay K. S. Chithra",
-  artist: "Artist: Unknown",
-  cover: "covers/Kehna_Hi_Kya_From_Bombay_K._S._Chithra.jpg"
+  cover: "covers/Karan_Aujla_-_For_a_reason.jpg"
 }
 ];
