d0fa4d6ef4f35bbcfd2bfa1a987108156e817903
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-09-18 06:21:58 +0530

    Auto update

--- Vanita/js/songs.js
+++ Vanita/js/songs.js
@@ -1,8 +1,56 @@
 const songs = [
 {
-  name: "Bismil_|_Haider.mp3",
-  title: "Bismil | Haider",
+  name: "Lucke_-_Paramavatar.mp3",
+  title: "Lucke - Paramavatar",
   artist: "Artist: Unknown",
-  cover: "covers/Bismil_|_Haider.jpg"
+  cover: "covers/Lucke_-_Paramavatar.jpg"
+},
+{
+  name: "Kishan_Soni_-_Mahabharat_Song_(Krishna_Arjuna_Rap_Song).mp3",
+  title: "Kishan Soni - Mahabharat Song (Krishna Arjuna Rap Song)",
+  artist: "Artist: Unknown",
+  cover: "covers/Kishan_Soni_-_Mahabharat_Song_(Krishna_Arjuna_Rap_Song).jpg"
+},
+{
+  name: "Shlovij_-_Main_He_Wo_Krishna.mp3",
+  title: "Shlovij - Main He Wo Krishna",
+  artist: "Artist: Unknown",
+  cover: "covers/Shlovij_-_Main_He_Wo_Krishna.jpg"
+},
+{
+  name: "Raanjha_-_Lakshman_Parshuram.mp3",
+  title: "Raanjha - Lakshman Parshuram",
+  artist: "Artist: Unknown",
+  cover: "covers/Raanjha_-_Lakshman_Parshuram.jpg"
+},
+{
+  name: "Raanjha_-_Mahavatar_Narsingh.mp3",
+  title: "Raanjha - Mahavatar Narsingh",
+  artist: "Artist: Unknown",
+  cover: "covers/Raanjha_-_Mahavatar_Narsingh.jpg"
+},
+{
+  name: "Lucke_-_Duvidha.mp3",
+  title: "Lucke - Duvidha",
+  artist: "Artist: Unknown",
+  cover: "covers/Lucke_-_Duvidha.jpg"
+},
+{
+  name: "Narci_-_Angad_Lalkaar.mp3",
+  title: "Narci - Angad Lalkaar",
+  artist: "Artist: Unknown",
+  cover: "covers/Narci_-_Angad_Lalkaar.jpg"
+},
+{
+  name: "Raanjha_-_Arjun.mp3",
+  title: "Raanjha - Arjun",
+  artist: "Artist: Unknown",
+  cover: "covers/Raanjha_-_Arjun.jpg"
+},
+{
+  name: "Red_Sky_Records_-_Wahi_Karn_Hu_Mai.mp3",
+  title: "Red Sky Records - Wahi Karn Hu Mai",
+  artist: "Artist: Unknown",
+  cover: "covers/Red_Sky_Records_-_Wahi_Karn_Hu_Mai.jpg"
 }
 ];
