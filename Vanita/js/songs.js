b21ffd7cc83faf82cad396078d3be2c6a54d6fcf
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-10-10 17:50:22 +0530

    Auto update

--- Vanita/js/songs.js
+++ Vanita/js/songs.js
@@ -1,3 +1,14 @@
 const songs = [
-
+{
+  name: "Sad_Gaana_(Animated_Video).mp3",
+  title: "Sad Gaana (Animated Video)",
+  artist: "Artist: Unknown",
+  cover: "covers/Sad_Gaana_(Animated_Video).jpg"
+},
+{
+  name: "Chatak.mp3",
+  title: "Chatak",
+  artist: "Artist: Unknown",
+  cover: "covers/Chatak.jpg"
+}
 ];
