021d8182b39ece05dd138f2f3021ebc493d0a799
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-11-06 04:58:43 +0530

    Auto update

--- Vanita/js/songs.js
+++ Vanita/js/songs.js
@@ -1,8 +1,8 @@
 const songs = [
 {
-  name: "Monica_-_COOLIE.mp3",
-  title: "Monica - COOLIE",
+  name: "YEAR-END_MEGAMIX_2024_|_4K_NIKHIL_MUSIQ_|.mp3",
+  title: "YEAR-END MEGAMIX 2024 | 4K NIKHIL MUSIQ |",
   artist: "Artist: Unknown",
-  cover: "covers/Monica_-_COOLIE.jpg"
+  cover: "covers/YEAR-END_MEGAMIX_2024_|_4K_NIKHIL_MUSIQ_|.jpg"
 }
 ];
