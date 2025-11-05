a3b2cb000be013954036e4d419244cd5ffbcf343
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-11-06 04:59:42 +0530

    Update Sscript.js

--- Sscript.js
+++ Sscript.js
@@ -18,6 +18,12 @@ const songs = [
   cover: "covers/Monica_-_COOLIE.jpg"
 },
 {
+  name: "YEAR-END_MEGAMIX_2024_|_4K_NIKHIL_MUSIQ_|.mp3",
+  title: "YEAR-END MEGAMIX 2024 | 4K NIKHIL MUSIQ |",
+  artist: "Artist: Unknown",
+  cover: "covers/YEAR-END_MEGAMIX_2024_|_4K_NIKHIL_MUSIQ_|.jpg"
+},
+{
   name: "TAK_TAK_DEKHO_NAACHRO_SANWARIYA_|_SAWARIYA_|_EKO_DASO_SUNDARI_SUNDARI.mp3",
   title: "TAK TAK DEKHO NAACHRO SANWARIYA | SAWARIYA | EKO DASO SUNDARI SUNDARI",
   artist: "Artist: Unknown",
