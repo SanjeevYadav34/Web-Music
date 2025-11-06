ee1dc9cdf08b0a09e6e60ee05ac7d3ec031a8680
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-11-06 18:17:01 +0530

    Auto update

--- Vanita/js/songs.js
+++ Vanita/js/songs.js
@@ -1,8 +1,8 @@
 const songs = [
 {
-  name: "Raftaar,_Rashmeet_Kaur_-_Ghana_Kasoota_(Lyrics).mp3",
-  title: "Raftaar, Rashmeet Kaur - Ghana Kasoota (Lyrics)",
+  name: "Odimaga_Lyrics_|_Aavesham_|Fahad_fazil_|_Sushin_shyam.mp3",
+  title: "Odimaga Lyrics | Aavesham |Fahad fazil | Sushin shyam",
   artist: "Artist: Unknown",
-  cover: "covers/Raftaar,_Rashmeet_Kaur_-_Ghana_Kasoota_(Lyrics).jpg"
+  cover: "covers/Odimaga_Lyrics_|_Aavesham_|Fahad_fazil_|_Sushin_shyam.jpg"
 }
 ];
