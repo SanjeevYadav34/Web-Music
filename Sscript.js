50a3f2f3d4036fa3f045f38d840ab573d15d1a9a
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-11-17 17:05:47 +0530

    Update Sscript.js

--- Sscript.js
+++ Sscript.js
@@ -12,6 +12,12 @@ const songListDiv = document.getElementById("songList");
 
 const songs = [
 {
+  name: "Gal_Sun_Lyrics_-_Sabat_Batin_ft._Rackstar_|_@VibePillMusic.mp3",
+  title: "Gal Sun Lyrics - Sabat Batin ft. Rackstar | @VibePillMusic",
+  artist: "Artist: Unknown",
+  cover: "covers/Gal_Sun_Lyrics_-_Sabat_Batin_ft._Rackstar_|_@VibePillMusic.jpg"
+},
+{
   name: "Monica_-_COOLIE.mp3",
   title: "Monica - COOLIE",
   artist: "Artist: Unknown",
