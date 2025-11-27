0412037920adaf25d10b2d6db17e19d08e1eacff
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-11-27 17:53:12 +0530

    Update Sscript.js

--- Sscript.js
+++ Sscript.js
@@ -30,6 +30,12 @@ const songs = [
   cover: "covers/Bargad_Arpit_Bala.jpg"
 },
 {
+  name: "Confess_Your_Love_Funk.mp3",
+  title: "Confess Your Love Funk",
+  artist: "Artist: Unknown",
+  cover: "covers/Confess_Your_Love_Funk.jpg"
+},
+{
   name: "Apun_Jaisa_Topori_Full_Song.mp3",
   title: "Apun Jaisa Topori Full Song",
   artist: "Artist: Unknown",
