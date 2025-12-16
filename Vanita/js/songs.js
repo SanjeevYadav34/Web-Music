84137f959523a191169e64c0c13c75234e2c51f3
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-12-16 06:41:38 +0530

    Auto update

--- Vanita/js/songs.js
+++ Vanita/js/songs.js
@@ -1,8 +1,62 @@
 const songs = [
 {
-  name: "Fakira_-_Lyrical.mp3",
-  title: "Fakira - Lyrical",
+  name: "KK_-_Pal.mp3",
+  title: "KK - Pal",
   artist: "Artist: Unknown",
-  cover: "covers/Fakira_-_Lyrical.jpg"
+  cover: "covers/KK_-_Pal.jpg"
+},
+{
+  name: "KK_-_Tadap_Tadap.mp3",
+  title: "KK - Tadap Tadap",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Tadap_Tadap.jpg"
+},
+{
+  name: "KK_-_Zara_Sa.mp3",
+  title: "KK - Zara Sa",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Zara_Sa.jpg"
+},
+{
+  name: "KK_-_Tu_Hi_Meri_Shab_Hai.mp3",
+  title: "KK - Tu Hi Meri Shab Hai",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Tu_Hi_Meri_Shab_Hai.jpg"
+},
+{
+  name: "KK_-_Khuda_Jaane.mp3",
+  title: "KK - Khuda Jaane",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Khuda_Jaane.jpg"
+},
+{
+  name: "KK_-_Aankhon_Mein_Teri.mp3",
+  title: "KK - Aankhon Mein Teri",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Aankhon_Mein_Teri.jpg"
+},
+{
+  name: "KK_-_Sach_Keh_Raha_Hai_Deewana.mp3",
+  title: "KK - Sach Keh Raha Hai Deewana",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Sach_Keh_Raha_Hai_Deewana.jpg"
+},
+{
+  name: "KK_-_Dil_Ibaadat.mp3",
+  title: "KK - Dil Ibaadat",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Dil_Ibaadat.jpg"
+},
+{
+  name: "KK_-_Labon_Ko.mp3",
+  title: "KK - Labon Ko",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Labon_Ko.jpg"
+},
+{
+  name: "KK_-_Alvida.mp3",
+  title: "KK - Alvida",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Alvida.jpg"
 }
 ];
