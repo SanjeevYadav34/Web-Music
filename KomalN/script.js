dccc0727cf342ca39f08daa260cba8df75fb396a
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-12-16 07:12:53 +0530

    Update script.js

--- KomalN/script.js
+++ KomalN/script.js
@@ -68,6 +68,66 @@ const songs = [
   title: "KK - Alvida",
   artist: "Artist: Unknown",
   cover: "covers/KK_-_Alvida.jpg"
+},
+{
+  name: "KK_-_O_Meri_Jaan.mp3",
+  title: "KK - O Meri Jaan",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_O_Meri_Jaan.jpg"
+},
+{
+  name: "KK_-_Kya_Mujhe_Pyaar_Hai.mp3",
+  title: "KK - Kya Mujhe Pyaar Hai",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Kya_Mujhe_Pyaar_Hai.jpg"
+},
+{
+  name: "KK_-_Ajab_Si.mp3",
+  title: "KK - Ajab Si",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Ajab_Si.jpg"
+},
+{
+  name: "KK_-_Beete_Lamhein.mp3",
+  title: "KK - Beete Lamhein",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Beete_Lamhein.jpg"
+},
+{
+  name: "KK_-_Tujhe_Sochta_Hoon.mp3",
+  title: "KK - Tujhe Sochta Hoon",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Tujhe_Sochta_Hoon.jpg"
+},
+{
+  name: "KK_-_Haan_Tu_Hain.mp3",
+  title: "KK - Haan Tu Hain",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Haan_Tu_Hain.jpg"
+},
+{
+  name: "KK_-_Mere_Bina.mp3",
+  title: "KK - Mere Bina",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Mere_Bina.jpg"
+},
+{
+  name: "KK_-_Zindagi_Do_Pal_Ki.mp3",
+  title: "KK - Zindagi Do Pal Ki",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Zindagi_Do_Pal_Ki.jpg"
+},
+{
+  name: "KK_-_Bas_Ek_Pal.mp3",
+  title: "KK - Bas Ek Pal",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Bas_Ek_Pal.jpg"
+},
+{
+  name: "KK_-_Awarapan_Banjarapan.mp3",
+  title: "KK - Awarapan Banjarapan",
+  artist: "Artist: Unknown",
+  cover: "covers/KK_-_Awarapan_Banjarapan.jpg"
 }
 ];
 
