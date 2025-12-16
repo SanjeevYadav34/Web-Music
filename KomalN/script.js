8fad96e2eae4a95348d9828ccfebfe2fd7e4bac6
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2025-12-16 06:44:53 +0530

    Update script.js

--- KomalN/script.js
+++ KomalN/script.js
@@ -10,178 +10,64 @@ const loopBtn = document.getElementById("loop");
 
 const songs = [
 {
-  name: "../../songs/TAK_TAK_DEKHO_NAACHRO_SANWARIYA_|_SAWARIYA_|_EKO_DASO_SUNDARI_SUNDARI.mp3",
-  title: "TAK TAK DEKHO NAACHRO SANWARIYA | SAWARIYA | EKO DASO SUNDARI SUNDARI",
+  name: "KK_-_Pal.mp3",
+  title: "KK - Pal",
   artist: "Artist: Unknown",
-  cover: "../covers/TAK_TAK_DEKHO_NAACHRO_SANWARIYA_|_SAWARIYA_|_EKO_DASO_SUNDARI_SUNDARI.jpg"
+  cover: "covers/KK_-_Pal.jpg"
 },
 {
-  name: "Darkhaast_-_Mithoon,_Arijit_Singh,_Sunidhi_Chauhan.mp3",
-  title: "Darkhaast - Mithoon, Arijit Singh, Sunidhi Chauhan",
+  name: "KK_-_Tadap_Tadap.mp3",
+  title: "KK - Tadap Tadap",
   artist: "Artist: Unknown",
-  cover: "covers/Darkhaast_-_Mithoon,_Arijit_Singh,_Sunidhi_Chauhan.jpg"
+  cover: "covers/KK_-_Tadap_Tadap.jpg"
 },
 {
-  name: "Khoya_Khoya_-_Sachin-Jigar,_Mohit_Chauhan,_Priya_S..mp3",
-  title: "Khoya Khoya - Sachin-Jigar, Mohit Chauhan, Priya S.",
+  name: "KK_-_Zara_Sa.mp3",
+  title: "KK - Zara Sa",
   artist: "Artist: Unknown",
-  cover: "covers/Khoya_Khoya_-_Sachin-Jigar,_Mohit_Chauhan,_Priya_S..jpg"
+  cover: "covers/KK_-_Zara_Sa.jpg"
 },
 {
-  name: "Khwab_-_Iqlipse_Nova,_Aditya_A.mp3",
-  title: "Khwab - Iqlipse Nova, Aditya A",
+  name: "KK_-_Tu_Hi_Meri_Shab_Hai.mp3",
+  title: "KK - Tu Hi Meri Shab Hai",
   artist: "Artist: Unknown",
-  cover: "covers/Khwab_-_Iqlipse_Nova,_Aditya_A.jpg"
+  cover: "covers/KK_-_Tu_Hi_Meri_Shab_Hai.jpg"
 },
 {
-  name: "I_Am_In_Love_-_Pritam,_KK,_Dominique,_Neelesh_Misra.mp3",
-  title: "I Am In Love - Pritam, KK, Dominique, Neelesh Misra",
+  name: "KK_-_Khuda_Jaane.mp3",
+  title: "KK - Khuda Jaane",
   artist: "Artist: Unknown",
-  cover: "covers/I_Am_In_Love_-_Pritam,_KK,_Dominique,_Neelesh_Misra.jpg"
+  cover: "covers/KK_-_Khuda_Jaane.jpg"
 },
 {
-  name: "Ik_Vaari_Aa_-_Pritam,_Arijit_Singh,_Amitabh_Bhattacharya.mp3",
-  title: "Ik Vaari Aa - Pritam, Arijit Singh, Amitabh Bhattacharya",
+  name: "KK_-_Aankhon_Mein_Teri.mp3",
+  title: "KK - Aankhon Mein Teri",
   artist: "Artist: Unknown",
-  cover: "covers/Ik_Vaari_Aa_-_Pritam,_Arijit_Singh,_Amitabh_Bhattacharya.jpg"
+  cover: "covers/KK_-_Aankhon_Mein_Teri.jpg"
 },
 {
-  name: "Dildaara_(Stand_By_Me)_-_Vishal-Shekhar,_Shafqat_Amanat_Ali.mp3",
-  title: "Dildaara (Stand By Me) - Vishal-Shekhar, Shafqat Amanat Ali",
+  name: "KK_-_Sach_Keh_Raha_Hai_Deewana.mp3",
+  title: "KK - Sach Keh Raha Hai Deewana",
   artist: "Artist: Unknown",
-  cover: "covers/Dildaara_(Stand_By_Me)_-_Vishal-Shekhar,_Shafqat_Amanat_Ali.jpg"
+  cover: "covers/KK_-_Sach_Keh_Raha_Hai_Deewana.jpg"
 },
 {
-  name: "Dil_Na_Jaaneya_-_Rochak_Kohli,_AKASA,_Lauv.mp3",
-  title: "Dil Na Jaaneya - Rochak Kohli, AKASA, Lauv",
+  name: "KK_-_Dil_Ibaadat.mp3",
+  title: "KK - Dil Ibaadat",
   artist: "Artist: Unknown",
-  cover: "covers/Dil_Na_Jaaneya_-_Rochak_Kohli,_AKASA,_Lauv.jpg"
+  cover: "covers/KK_-_Dil_Ibaadat.jpg"
 },
 {
-  name: "Tumse_Hi_Tumse_-_Vishal-Shekhar,_Shekhar_Ravjiani,_Caralisa.mp3",
-  title: "Tumse Hi Tumse - Vishal-Shekhar, Shekhar Ravjiani, Caralisa",
+  name: "KK_-_Labon_Ko.mp3",
+  title: "KK - Labon Ko",
   artist: "Artist: Unknown",
-  cover: "covers/Tumse_Hi_Tumse_-_Vishal-Shekhar,_Shekhar_Ravjiani,_Caralisa.jpg"
+  cover: "covers/KK_-_Labon_Ko.jpg"
 },
 {
-  name: "Ajab_Leher_-_Vishal-Shekhar,_Neeraj_Shridhar.mp3",
-  title: "Ajab Leher - Vishal-Shekhar, Neeraj Shridhar",
+  name: "KK_-_Alvida.mp3",
+  title: "KK - Alvida",
   artist: "Artist: Unknown",
-  cover: "covers/Ajab_Leher_-_Vishal-Shekhar,_Neeraj_Shridhar.jpg"
-},
-{
-  name: "Khabar_Nahi_-_Vishal-Shekhar,_Shreya_Ghoshal.mp3",
-  title: "Khabar Nahi - Vishal-Shekhar, Shreya Ghoshal",
-  artist: "Artist: Unknown",
-  cover: "covers/Khabar_Nahi_-_Vishal-Shekhar,_Shreya_Ghoshal.jpg"
-},
-{
-  name: "Pee_Loon_-_Pritam,_Mohit_Chauhan,_Irshad_Kamil.mp3",
-  title: "Pee Loon - Pritam, Mohit Chauhan, Irshad Kamil",
-  artist: "Artist: Unknown",
-  cover: "covers/Pee_Loon_-_Pritam,_Mohit_Chauhan,_Irshad_Kamil.jpg"
-},
-{
-  name: "Lamhey_-_Anubha_Bajaj.mp3",
-  title: "Lamhey - Anubha Bajaj",
-  artist: "Artist: Unknown",
-  cover: "covers/Lamhey_-_Anubha_Bajaj.jpg"
-},
-{
-  name: "IRAADAY_-_ZAYEM.mp3",
-  title: "IRAADAY - ZAYEM",
-  artist: "Artist: Unknown",
-  cover: "covers/IRAADAY_-_ZAYEM.jpg"
-},
-{
-  name: "Tere_Bina_-_Zaeden.mp3",
-  title: "Tere Bina - Zaeden",
-  artist: "Artist: Unknown",
-  cover: "covers/Tere_Bina_-_Zaeden.jpg"
-},
-{
-  name: "Sajde_-_Pritam,_KK,_Sunidhi_Chauhan.mp3",
-  title: "Sajde - Pritam, KK, Sunidhi Chauhan",
-  artist: "Artist: Unknown",
-  cover: "covers/Sajde_-_Pritam,_KK,_Sunidhi_Chauhan.jpg"
-},
-{
-  name: "Sajde_-_Shankar-Ehsaan-Loy,_Arijit_Singh,_Nihira_Joshi.mp3",
-  title: "Sajde - Shankar-Ehsaan-Loy, Arijit Singh, Nihira Joshi",
-  artist: "Artist: Unknown",
-  cover: "covers/Sajde_-_Shankar-Ehsaan-Loy,_Arijit_Singh,_Nihira_Joshi.jpg"
-},
-{
-  name: "Oh_Girl_You’re_Mine_-_Tarun_Sagar,_Alyssa_Mendonsa,_Loy_Mendonsa.mp3",
-  title: "Oh Girl You’re Mine - Tarun Sagar, Alyssa Mendonsa, Loy Mendonsa",
-  artist: "Artist: Unknown",
-  cover: "covers/Oh_Girl_You’re_Mine_-_Tarun_Sagar,_Alyssa_Mendonsa,_Loy_Mendonsa.jpg"
-},
-{
-  name: "Caller_Tune_-_Neeraj_Shridhar,_Neeti_Mohan.mp3",
-  title: "Caller Tune - Neeraj Shridhar, Neeti Mohan",
-  artist: "Artist: Unknown",
-  cover: "covers/Caller_Tune_-_Neeraj_Shridhar,_Neeti_Mohan.jpg"
-},
-{
-  name: "You_Get_Me_Rockin_&_Reeling_-_Pritam,_Neeraj_Shridhar.mp3",
-  title: "You Get Me Rockin & Reeling - Pritam, Neeraj Shridhar",
-  artist: "Artist: Unknown",
-  cover: "covers/You_Get_Me_Rockin_&_Reeling_-_Pritam,_Neeraj_Shridhar.jpg"
-},
-{
-  name: "Katchi_Sera.mp3",
-  title: "Katchi Sera",
-  artist: "Artist: Unknown",
-  cover: "covers/Katchi_Sera.jpg"
-},
-{
-  name: "Dheere_Dheere.mp3",
-  title: "Dheere Dheere",
-  artist: "Artist: Unknown",
-  cover: "covers/Dheere_Dheere.jpg"
-},
-{
-  name: "Chuttamulle.mp3",
-  title: "Chuttamulle",
-  artist: "Artist: Unknown",
-  cover: "covers/Chuttamulle.jpg"
-},
-{
-  name: "Paththavaikkum.mp3",
-  title: "Paththavaikkum",
-  artist: "Artist: Unknown",
-  cover: "covers/Paththavaikkum.jpg"
-},
-{
-  name: "Fakira.mp3",
-  title: "Fakira",
-  artist: "Artist: Unknown",
-  cover: "covers/Fakira.jpg"
-},
-{
-  name: "Sanam_Puri_-_Aashiq_Tera.mp3",
-  title: "Sanam Puri - Aashiq Tera",
-  artist: "Artist: Unknown",
-  cover: "covers/Sanam_Puri_-_Aashiq_Tera.jpg"
-},
-{
-  name: "Alisha.mp3",
-  title: "Alisha",
-  artist: "Artist: Unknown",
-  cover: "covers/Alisha.jpg"
-},
-{
-  name: "I_Think_This_Is_Love.mp3",
-  title: "I Think This Is Love",
-  artist: "Artist: Unknown",
-  cover: "covers/I_Think_This_Is_Love.jpg"
-},
-{
-  name: "Soniyo.mp3",
-  title: "Soniyo",
-  artist: "Artist: Unknown",
-  cover: "covers/Soniyo.jpg"
+  cover: "covers/KK_-_Alvida.jpg"
 }
 ];
 
