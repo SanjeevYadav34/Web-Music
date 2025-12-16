9ccf44fb6639549af94715446f0bf8ac57376a21
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2025-12-16 07:10:00 +0530

    Auto update

--- Vanita/js/songs.js
+++ Vanita/js/songs.js
@@ -1,62 +1,62 @@
 const songs = [
 {
-  name: "KK_-_Pal.mp3",
-  title: "KK - Pal",
+  name: "KK_-_O_Meri_Jaan.mp3",
+  title: "KK - O Meri Jaan",
   artist: "Artist: Unknown",
-  cover: "covers/KK_-_Pal.jpg"
+  cover: "covers/KK_-_O_Meri_Jaan.jpg"
 },
 {
-  name: "KK_-_Tadap_Tadap.mp3",
-  title: "KK - Tadap Tadap",
+  name: "KK_-_Kya_Mujhe_Pyaar_Hai.mp3",
+  title: "KK - Kya Mujhe Pyaar Hai",
   artist: "Artist: Unknown",
-  cover: "covers/KK_-_Tadap_Tadap.jpg"
+  cover: "covers/KK_-_Kya_Mujhe_Pyaar_Hai.jpg"
 },
 {
-  name: "KK_-_Zara_Sa.mp3",
-  title: "KK - Zara Sa",
+  name: "KK_-_Ajab_Si.mp3",
+  title: "KK - Ajab Si",
   artist: "Artist: Unknown",
-  cover: "covers/KK_-_Zara_Sa.jpg"
+  cover: "covers/KK_-_Ajab_Si.jpg"
 },
 {
-  name: "KK_-_Tu_Hi_Meri_Shab_Hai.mp3",
-  title: "KK - Tu Hi Meri Shab Hai",
+  name: "KK_-_Beete_Lamhein.mp3",
+  title: "KK - Beete Lamhein",
   artist: "Artist: Unknown",
-  cover: "covers/KK_-_Tu_Hi_Meri_Shab_Hai.jpg"
+  cover: "covers/KK_-_Beete_Lamhein.jpg"
 },
 {
-  name: "KK_-_Khuda_Jaane.mp3",
-  title: "KK - Khuda Jaane",
+  name: "KK_-_Tujhe_Sochta_Hoon.mp3",
+  title: "KK - Tujhe Sochta Hoon",
   artist: "Artist: Unknown",
-  cover: "covers/KK_-_Khuda_Jaane.jpg"
+  cover: "covers/KK_-_Tujhe_Sochta_Hoon.jpg"
 },
 {
-  name: "KK_-_Aankhon_Mein_Teri.mp3",
-  title: "KK - Aankhon Mein Teri",
+  name: "KK_-_Haan_Tu_Hain.mp3",
+  title: "KK - Haan Tu Hain",
   artist: "Artist: Unknown",
-  cover: "covers/KK_-_Aankhon_Mein_Teri.jpg"
+  cover: "covers/KK_-_Haan_Tu_Hain.jpg"
 },
 {
-  name: "KK_-_Sach_Keh_Raha_Hai_Deewana.mp3",
-  title: "KK - Sach Keh Raha Hai Deewana",
+  name: "KK_-_Mere_Bina.mp3",
+  title: "KK - Mere Bina",
   artist: "Artist: Unknown",
-  cover: "covers/KK_-_Sach_Keh_Raha_Hai_Deewana.jpg"
+  cover: "covers/KK_-_Mere_Bina.jpg"
 },
 {
-  name: "KK_-_Dil_Ibaadat.mp3",
-  title: "KK - Dil Ibaadat",
+  name: "KK_-_Zindagi_Do_Pal_Ki.mp3",
+  title: "KK - Zindagi Do Pal Ki",
   artist: "Artist: Unknown",
-  cover: "covers/KK_-_Dil_Ibaadat.jpg"
+  cover: "covers/KK_-_Zindagi_Do_Pal_Ki.jpg"
 },
 {
-  name: "KK_-_Labon_Ko.mp3",
-  title: "KK - Labon Ko",
+  name: "KK_-_Bas_Ek_Pal.mp3",
+  title: "KK - Bas Ek Pal",
   artist: "Artist: Unknown",
-  cover: "covers/KK_-_Labon_Ko.jpg"
+  cover: "covers/KK_-_Bas_Ek_Pal.jpg"
 },
 {
-  name: "KK_-_Alvida.mp3",
-  title: "KK - Alvida",
+  name: "KK_-_Awarapan_Banjarapan.mp3",
+  title: "KK - Awarapan Banjarapan",
   artist: "Artist: Unknown",
-  cover: "covers/KK_-_Alvida.jpg"
+  cover: "covers/KK_-_Awarapan_Banjarapan.jpg"
 }
 ];
