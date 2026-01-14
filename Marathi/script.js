94d4383dadb6b8787d87bfa694361c0ca0b23818
Sanjeev Yadav <107674434+SanjeevYadav34@users.noreply.github.com>
2026-01-15 04:51:40 +0530

    Update script.js

--- Marathi/script.js
+++ Marathi/script.js
@@ -14,6 +14,12 @@ const songs = [
   title: "CHAKAR SHIVBACH HONAR",
   artist: "Artist: Unknown",
   cover: "covers/CHAKAR_SHIVBACH_HONAR.jpg"
+},
+{
+  name: "songs/Lallati_Bhandar,_Full_Video_-_Jogwa_|_Ajay-Atul_|_Mukta_Barve_|_Upendra_Limaye_|_Kishor_Kadam.mp3",
+  title: "Lallati Bhandar, Full Video - Jogwa | Ajay-Atul | Mukta Barve | Upendra Limaye | Kishor Kadam",
+  artist: "Artist: Unknown",
+  cover: "covers/Lallati_Bhandar,_Full_Video_-_Jogwa_|_Ajay-Atul_|_Mukta_Barve_|_Upendra_Limaye_|_Kishor_Kadam.jpg"
 }
 ];
 
