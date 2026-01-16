8dc24ac9d939a32bcb8fb2a35fdcbcba3f3f78de
SanjeevYadav34 <sanjeevyadav4696@gmail.com>
2026-01-16 23:22:40 +0530

    Auto update

--- Vanita/js/songs.js
+++ Vanita/js/songs.js
@@ -1,8 +1,8 @@
 const songs = [
 {
-  name: "Lallati_Bhandar,_Full_Video_-_Jogwa_|_Ajay-Atul_|_Mukta_Barve_|_Upendra_Limaye_|_Kishor_Kadam.mp3",
-  title: "Lallati Bhandar, Full Video - Jogwa | Ajay-Atul | Mukta Barve | Upendra Limaye | Kishor Kadam",
+  name: "Chandi_O_Meri_Chandi.mp3",
+  title: "Chandi O Meri Chandi",
   artist: "Artist: Unknown",
-  cover: "covers/Lallati_Bhandar,_Full_Video_-_Jogwa_|_Ajay-Atul_|_Mukta_Barve_|_Upendra_Limaye_|_Kishor_Kadam.jpg"
+  cover: "covers/Chandi_O_Meri_Chandi.jpg"
 }
 ];
